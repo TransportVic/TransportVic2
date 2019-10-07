a2c5141c692a1b03a2d7ced79ac55f983129a733
Eyeball Edward <edwardyeung.private@gmail.com>
2019-10-07 19:09:44 +1100

    safari ._.

--- application/static/scripts/mockups/reload-fss-escalator.js
+++ application/static/scripts/mockups/reload-fss-escalator.js
@@ -97,14 +97,13 @@ setInterval(() => {
       let n
       let stopCount = firstDeparture.additionalInfo.screenStops.length
 
-      if (stopCount >= 31) n = 24
-      if (stopCount >= 28) n = 20
+      if (stopCount >= 32) n = 20
       else if (stopCount >= 24) n = 18 // honestly idk how tf this works
       else if (stopCount >= 20) n = 17
       else if (stopCount >= 12) n = 16 // ?? should really have checked but aw - moorabbin shorts
       // else if (stopCount >= 12) n = 12
       else n = 12
-
+      
       let firstNStations = firstDeparture.additionalInfo.screenStops.slice(0, n)
       let nextNStations = firstDeparture.additionalInfo.screenStops.slice(n)
 
