0058d4af721c7e82732ba2765b4f0ed2aae0b689
Eyeball Edward <edwardyeung.private@gmail.com>
2019-10-12 09:28:05 +1100

    whoops fix mtm dir on gtfs timetable

--- load-gtfs/metro-trains/load-all.sh
+++ load-gtfs/metro-trains/load-all.sh
@@ -1,5 +1,5 @@
 node load-gtfs/metro-trains/load-metro-rail-stations.js
 node load-gtfs/metro-trains/load-metro-rail-routes.js
-node load-gtfs/metro-trains/load-metro-gtfs-timetables.js
+node --max-old-space-size=1024 load-gtfs/metro-trains/load-metro-gtfs-timetables.js
 node load-gtfs/metro-trains/load-metro-route-stops.js
 node load-gtfs/metro-trains/load-metro-static-timetables.js
