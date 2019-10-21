32d796e596b5b45564a360449476f75849fe073e
Eyeball Edward <edwardyeung.private@gmail.com>
2019-10-21 11:00:28 +1100

    add regional coach stop

--- load-gtfs/regional-coach/load-regional-coach-stops.js
+++ load-gtfs/regional-coach/load-regional-coach-stops.js
@@ -2,6 +2,7 @@ const DatabaseConnection = require('../../database/DatabaseConnection')
 const config = require('../../config.json')
 const utils = require('../../utils')
 const fs = require('fs')
+const async = require('async')
 const loadStops = require('../utils/load-stops')
 const { createStopsLookup } = require('../utils/datamart-utils')
 const stopsData = utils.parseGTFSData(fs.readFileSync('gtfs/5/stops.txt').toString())
@@ -17,6 +18,31 @@ database.connect({
 
   let stopsLookup = createStopsLookup(datamartStops)
   let stopCount = await loadStops(stopsData, stops, 'regional coach', stopsLookup)
+
+  let allVlineStations = await stops.findDocuments({
+    bays: {
+      $elemMatch: {
+        mode: 'regional train'
+      }
+    }
+  }).toArray()
+
+  await async.forEach(allVlineStations, async station => {
+    let hasCoachStop = station.bays.filter(bay => {
+      return bay.mode === 'regional coach' && bay.fullStopName.endsWith('Railway Station')
+    }).length !== 0
+    let stationPlatform = station.bays.filter(bay => bay.mode === 'regional train')[0]
+
+    if (!hasCoachStop) {
+      let coachStop = JSON.parse(JSON.stringify(stationPlatform))
+      coachStop.mode = 'regional coach'
+      station.bays.push(coachStop)
+      await stops.updateDocument({ _id: station._id }, {
+        $set: station
+      })
+      stopCount++
+    }
+  })
 
   console.log('Completed loading in ' + stopCount + ' V/Line coach stops')
   process.exit()
