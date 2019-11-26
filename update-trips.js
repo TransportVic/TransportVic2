1a6b25d14e77566f0d9510fc001b5dc490c08e01
Eyeball Edward <edwardyeung.private@gmail.com>
2019-11-26 23:05:58 +1100

    tracker merging

---
+++ update-trips.js
@@ -0,0 +1,35 @@
+const DatabaseConnection = require('./database/DatabaseConnection')
+const config = require('./config.json')
+const utils = require('./utils')
+const async = require('async')
+
+const database = new DatabaseConnection(config.databaseURL, config.databaseName)
+
+database.connect(async err => {
+  let busTrips = database.getCollection('bus trips')
+  let allTrips = await busTrips.findDocuments().toArray()
+  await async.forEach(allTrips, async trip => {
+    let {date, routeGTFSID, origin, destination, departureTime, destinationArrivalTime} = trip
+    let key = {
+      date, routeGTFSID, origin, destination, departureTime, destinationArrivalTime
+    }
+
+    let newOrigin = utils.adjustStopname(origin),
+        newDestination = utils.adjustStopname(destination)
+
+    if (newOrigin == origin && newDestination == destination) return
+
+    if (await busTrips.findDocument({
+      date, routeGTFSID, departureTime, destinationArrivalTime,
+      origin: newOrigin, destination: newDestination
+    })) {
+      await busTrips.deleteDocument({_id: trip._id})
+    } else {
+      trip.origin = newOrigin
+      trip.destination = newDestination
+      await busTrips.replaceDocument(key, trip)
+    }
+  })
+  console.log('Completed updating ' + allTrips.length + ' trips')
+  process.exit(0)
+})
