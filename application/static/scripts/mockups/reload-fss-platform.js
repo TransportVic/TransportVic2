e889e07f69cc885f2127393bdc6650ac27898133
Eyeball Edward <edwardyeung.private@gmail.com>
2020-05-06 17:26:57 +1000

    clear currentpattern when setting to arrival

--- application/static/scripts/mockups/reload-fss-platform.js
+++ application/static/scripts/mockups/reload-fss-platform.js
@@ -95,6 +95,7 @@ function setArrival() {
     $('.stoppingPattern').className = 'stoppingPattern stoppingAt arrivalContainer'
     setMessageActive(false)
     currentlyDisplaying = 'arrival'
+    currentPattern = ''
   }
 }
 
