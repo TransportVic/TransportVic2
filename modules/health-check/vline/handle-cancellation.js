c017627ae7c66e52fb8c264a38d7a7e8378665f2
Eyeball Edward <edwardyeung.private@gmail.com>
2020-05-12 10:01:47 +1000

    the ... - shepparton has been cancelled

--- modules/health-check/vline/handle-cancellation.js
+++ modules/health-check/vline/handle-cancellation.js
@@ -49,7 +49,7 @@ async function setServicesAsCancelled(db, services) {
 }
 
 async function cancellation(db, text) {
-  let service = text.match(/(\d{1,2}[:.]\d{1,2}) ([\w ]*?) (?:to|-) ([\w ]*?) (?:service|train|will)/)
+  let service = text.match(/(\d{1,2}[:.]\d{1,2}) ([\w ]*?) (?:to|-) ([\w ]*?) (?:service|train|will|has)/)
   let matches = []
 
   if (service) {
