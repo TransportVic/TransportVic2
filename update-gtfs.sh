cdee538ff775b81227dace6f3356504307795fab
Eyeball Edward <edwardyeung.private@gmail.com>
2020-05-15 12:08:14 +1000

    copy gtfs zips to OTP directory

--- update-gtfs.sh
+++ update-gtfs.sh
@@ -5,12 +5,14 @@ unzip gtfs.zip
 for i in {1..8}; do
   cd $i
   unzip google_transit.zip
+  cp google_transit.zip ../../../OpenTripPlanner/google_transit_${i}.zip
   cd ..
 done
 
 for i in {10..11}; do
   cd $i
   unzip google_transit.zip
+  cp google_transit.zip ../../../OpenTripPlanner/google_transit_${i}.zip
   cd ..
 done
 rm gtfs.zip
