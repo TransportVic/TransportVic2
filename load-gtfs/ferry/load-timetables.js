7506427668abcd2fc68087b553fd2400732b099c
Eyeball Edward <edwardyeung.private@gmail.com>
2020-05-27 17:03:39 +1000

    load in all ferry timetables

--- load-gtfs/ferry/load-timetables.js
+++ load-gtfs/ferry/load-timetables.js
@@ -124,7 +124,7 @@ function loadWesternPort($) {
         let timingParts = stop.departureTime.match(/(\d{1,2})[.:]?(\d{1,2})?/)
         let hour = parseInt(timingParts[1])
         let minute = parseInt(timingParts[2] || '0')
-        if (stop.departureTime.includes('pm')) hour += 12
+        if (stop.departureTime.includes('pm') && hour !== 12) hour += 12
 
         let gtfsTime = `${hour < 10 ? '0' + hour : hour}:${minute < 10 ? '0' + minute : minute}:00`
 
@@ -169,6 +169,187 @@ function loadWesternPort($) {
   return { trips: allTrips, timings: allTimings }
 }
 
+function loadWestgatePunt($) {
+  let lists = Array.from($('div#e820 div.mceTmpl > ul'))
+  let hTrips = [lists[2], lists[3]].reduce((a, e) => a.concat(Array.from($('li', e)).map(f => $(f).text())), [])
+  let rTrips = [lists[0], lists[1]].reduce((a, e) => a.concat(Array.from($('li', e)).map(f => $(f).text())), [])
+
+  let allTrips = []
+  let allTimings = []
+
+  function processTrips(departureTimes, headsign, gtfsDirection) {
+    let shapeType = `9-PMW-mjp.1.1.${gtfsDirection === '0' ? 'H' : 'R'}`
+
+    departureTimes.forEach(departureTime => {
+      let timingParts = departureTime.match(/(\d{1,2})[.:]?(\d{1,2})?/)
+      let hour = parseInt(timingParts[1])
+      let minute = parseInt(timingParts[2] || '0')
+      if (departureTime.includes('pm') && hour !== 12) hour += 12
+      let departureTimeMinutes = hour * 60 + minute
+
+      let departureGTFSTime = `${hour < 10 ? '0' + hour : hour}:${minute < 10 ? '0' + minute : minute}:00`
+
+      let arrivalHour = hour, arrivalMinute = minute
+      arrivalMinute += 4
+      if (arrivalMinute >= 60) {
+        arrivalMinute %= 60
+        arrivalHour++
+      }
+
+      let arrivalGTFSTime = `${arrivalHour < 10 ? '0' + arrivalHour : arrivalHour}:${arrivalMinute < 10 ? '0' + arrivalMinute : arrivalMinute}:00`
+      let tripID = `${departureTimeMinutes}.EVERYDAY.${shapeType}`
+
+      allTrips.push({
+        mode: 'ferry',
+        tripID,
+        routeGTFSID: '9-PMW',
+        calendarID: 'EVERYDAY',
+        gtfsDirection,
+        shapeID: shapeType,
+        headsign
+      })
+
+      let originGTFSID, destinationGTFSID
+      if (gtfsDirection === '0') { // port melbourne - spotwood
+        originGTFSID = 9000004
+        destinationGTFSID = 9000005
+      } else {
+        originGTFSID = 9000005
+        destinationGTFSID = 9000004
+      }
+
+      allTimings.push({
+        tripID,
+        stopTimings: [{
+          stopGTFSID: originGTFSID,
+          arrivalTime: departureGTFSTime,
+          departureTime: departureGTFSTime,
+          stopConditions: {
+            pickup: 0,
+            dropoff: 0
+          },
+          stopDistance: 0,
+          stopSequence: 0
+        }, {
+          stopGTFSID: destinationGTFSID,
+          arrivalTime: arrivalGTFSTime,
+          departureTime: arrivalGTFSTime,
+          stopConditions: {
+            pickup: 0,
+            dropoff: 0
+          },
+          stopDistance: 0,
+          stopSequence: 0
+        }]
+      })
+    })
+  }
+
+  processTrips(hTrips, 'Spotswood Ferry Terminal', '0')
+  processTrips(rTrips, 'Port Melbourne Ferry Terminal', '1')
+
+  return { trips: allTrips, timings: allTimings }
+}
+
+function loadPortPhillip($) {
+  let tables = Array.from($('div#e937 > table'))
+
+  let dayType = {
+    'Monday and Tuesday': 'WEEKDAY_MTue',
+    'Wednesday, Thursday and Friday': 'WEEKDAY_WThuF',
+    'Saturday and Sunday': 'WEEKEND_SatSun',
+    'Monday to Friday': 'WEEKDAY_MF',
+    'Monday, Tuesday and Wednesday': 'WEEKDAY_MTueW',
+    'Thursday and Friday': 'WEEKDAY_ThuF'
+  }
+
+  let portarlington = tables[0], geelong = tables[1]
+
+  function getGTFSTime(time) {
+    let timingParts = time.match(/(\d{1,2})[.:]?(\d{1,2})?/)
+    let hour = parseInt(timingParts[1])
+    let minute = parseInt(timingParts[2] || '0')
+    if (time.includes('pm') && hour !== 12) hour += 12
+    return `${hour < 10 ? '0' + hour : hour}:${minute < 10 ? '0' + minute : minute}:00`
+  }
+
+  let allTrips = [], allTimings = []
+
+  function processTrips(table, outHeadsign, routeGTFSID, outStopGTFSID) {
+    let rows = Array.from($('tr[style="height: 19px;"]', table))
+    let lastCalendarType = ''
+
+    let isOut = false
+    let gtfsDirection = '1'
+    rows.forEach((row, i) => {
+      let header = $('th', row).text().trim()
+      if (header) {
+        lastCalendarType = dayType[header]
+        if (lastCalendarType == 'WEEKDAY_MF') {
+          isOut = true
+          gtfsDirection = '0'
+        }
+      }
+      let timings = Array.from($('td', row)).map(e => getGTFSTime($(e).text()))
+      let headsign = isOut ? outHeadsign : 'Docklands Ferry Terminal'
+      let departureTimeParts = timings[0].split(':')
+      let departureTimeMinutes = departureTimeParts[0] * 60 + departureTimeParts[1] * 1
+
+      let shapeType = `9-${routeGTFSID}-mjp.1.1.${gtfsDirection === '0' ? 'H' : 'R'}`
+      let tripID = `${departureTimeMinutes}.${lastCalendarType}.${shapeType}`
+
+      allTrips.push({
+        mode: 'ferry',
+        tripID,
+        routeGTFSID: `9-${routeGTFSID}`,
+        calendarID: lastCalendarType,
+        gtfsDirection,
+        shapeID: shapeType,
+        headsign
+      })
+
+      let originGTFSID, destinationGTFSID
+      if (gtfsDirection === '0') { // port melbourne - spotwood
+        originGTFSID = 9000006
+        destinationGTFSID = outStopGTFSID
+      } else {
+        originGTFSID = outStopGTFSID
+        destinationGTFSID = 9000006
+      }
+
+      allTimings.push({
+        tripID,
+        stopTimings: [{
+          stopGTFSID: originGTFSID,
+          arrivalTime: timings[0],
+          departureTime: timings[0],
+          stopConditions: {
+            pickup: 0,
+            dropoff: 0
+          },
+          stopDistance: 0,
+          stopSequence: 0
+        }, {
+          stopGTFSID: destinationGTFSID,
+          arrivalTime: timings[1],
+          departureTime: timings[1],
+          stopConditions: {
+            pickup: 0,
+            dropoff: 0
+          },
+          stopDistance: 0,
+          stopSequence: 0
+        }]
+      })
+    })
+  }
+
+  processTrips(portarlington, 'Portarlington Ferry Terminal', 'PPO', 9000007)
+  // processTrips(geelong, 'Geelong Ferry Terminal', 'PGL', 9000008)
+
+  return { trips: allTrips, timings: allTimings }
+}
+
 database.connect({
   poolSize: 100
 }, async err => {
@@ -196,6 +377,16 @@ database.connect({
   totalCount += westernPortData.trips.length
 
   await loadGTFSTimetables({gtfsTimetables, stops, routes}, gtfsID, westernPortData.trips, westernPortData.timings, calendarDays, calendarDates)
+
+  let westgatePunt = loadWestgatePunt($)
+  totalCount += westgatePunt.trips.length
+
+  await loadGTFSTimetables({gtfsTimetables, stops, routes}, gtfsID, westgatePunt.trips, westgatePunt.timings, calendarDays, calendarDates)
+
+  let portPhillip = loadPortPhillip($)
+  totalCount += portPhillip.trips.length
+
+  await loadGTFSTimetables({gtfsTimetables, stops, routes}, gtfsID, portPhillip.trips, portPhillip.timings, calendarDays, calendarDates)
 
   await updateStats('ferry-timetables', totalCount)
   console.log('Completed loading in ' + totalCount + ' ferry timetables')
