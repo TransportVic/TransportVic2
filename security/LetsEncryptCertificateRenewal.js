30033aa513ff0114504196995467f0630621e4af
Eyeball Edward <edwardyeung.private@gmail.com>
2020-05-30 14:06:59 +1000

    update misc stuff

--- security/LetsEncryptCertificateRenewal.js
+++ security/LetsEncryptCertificateRenewal.js
@@ -4,14 +4,14 @@ const HTTPSServer = require('../server/HTTPSServer')
 const config = require('../config.json')
 
 function renew() {
-    console.log(new Date(), 'renewing certs')
-    childProcess.exec('certbot renew --non-interactive -a manual --preferred-challenges dns --manual-auth-hook "/home/ec2-user/certbot-hook.sh auth" --manual-cleanup-hook "/home/ec2-user/certbot-hook.sh cleanup"', function(err, stdout, stderr) {
-        stdout = stdout.toString().trim()
-        stderr = stderr.toString().trim()
-        if (stdout) console.log(stdout)
-        if (stderr) console.error(stderr)
-        HTTPSServer.createSecureContext(config.sslCertPath)
-    })
+  console.log('renewing certs')
+  childProcess.exec(`certbot renew ${config.certbotFlags}`, function(err, stdout, stderr) {
+    stdout = stdout.toString().trim()
+    stderr = stderr.toString().trim()
+    if (stdout) console.log(stdout)
+    if (stderr) console.error(stderr)
+    HTTPSServer.createSecureContext(config.sslCertPath)
+  })
 }
 
 setInterval(renew, 1000 * 60 * 60 * 12)
