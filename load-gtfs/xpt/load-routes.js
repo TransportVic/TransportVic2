54a6676e7a79b0f0c2c3e7b30b9d86e8717a8b65
Eyeball Edward <edwardyeung.private@gmail.com>
2020-06-14 00:04:13 +1000

    xpt timings loaded in

---
+++ load-gtfs/xpt/load-routes.js
@@ -0,0 +1,35 @@
+/*
+Route guide;
+13-XPT.H Sydney - Melbourne
+13-XPT.R Melbourne - Sydney
+*/
+
+const fs = require('fs')
+const path = require('path')
+const async = require('async')
+const DatabaseConnection = require('../../database/DatabaseConnection')
+const config = require('../../config.json')
+const loadRoutes = require('../utils/load-routes')
+const utils = require('../../utils')
+
+const database = new DatabaseConnection(config.databaseURL, config.databaseName)
+const updateStats = require('../utils/stats')
+
+let shapes = require('./data/shapes.json')
+
+let gtfsID = '13'
+
+database.connect({
+  poolSize: 100
+}, async err => {
+  let routes = database.getCollection('routes')
+  let routeData = utils.parseGTFSData(fs.readFileSync(path.join(__dirname, 'data', 'routes.txt')).toString())
+
+  await loadRoutes(routes, gtfsID, routeData, shapes, (routeGTFSID) => {
+    return ['Sydney Trains']
+  })
+
+  await updateStats('xpt-routes', routeData.length)
+  console.log('Completed loading in ' + routeData.length + ' xpt routes')
+  process.exit()
+})
---
+++ load-gtfs/xpt/load-stops.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+const async = require('async')
+const DatabaseConnection = require('../../database/DatabaseConnection')
+const config = require('../../config.json')
+const loadStops = require('../utils/load-stops')
+
+const database = new DatabaseConnection(config.databaseURL, config.databaseName)
+const updateStats = require('../utils/stats')
+
+let stopsData = require('./data/stops.json')
+
+let gtfsID = 13
+
+database.connect({
+  poolSize: 100
+}, async err => {
+  let stops = database.getCollection('stops')
+  await loadStops(stops, stopsData, {})
+
+  await updateStats('xpt-stations', stopsData.length)
+  console.log('Completed loading in ' + stopsData.length + ' xpt stations')
+  process.exit()
+})
