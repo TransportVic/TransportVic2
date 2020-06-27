b4b1229bc4ca25068891dcaafd87de2710c2c96a
Eyeball Edward <edwardyeung.private@gmail.com>
2020-06-27 15:33:46 +1000

    vline tracker fix

--- modules/vline-tracker.js
+++ modules/vline-tracker.js
@@ -86,6 +86,8 @@ async function getVNETDepartures(direction, db) {
     const originStation = await getStationFromVNETName(originVNETName, db)
     const destinationStation = await getStationFromVNETName(destinationVNETName, db)
 
+    if (!originStation || !destinationStation) return // Apparently origin or dest is sometimes unknown
+
     let originVLinePlatform = originStation.bays.find(bay => bay.mode === 'regional train')
     let destinationVLinePlatform = destinationStation.bays.find(bay => bay.mode === 'regional train')
 
