65c9b83743517d9b35e157960a6d0a6ba242731b
Eyeball Edward <edwardyeung.private@gmail.com>
2020-06-27 20:52:51 +1000

    connections

--- load-gtfs/regional-coach/extra/load-connections.js
+++ load-gtfs/regional-coach/extra/load-connections.js
@@ -5,7 +5,7 @@ const utils = require('../../../utils')
 
 const updateStats = require('../../utils/stats')
 
-let gtfsTimetables
+let stops, gtfsTimetables
 
 const database = new DatabaseConnection(config.databaseURL, config.databaseName)
 let mode = {
@@ -13,67 +13,96 @@ let mode = {
 }
 
 async function findConnections(changeoverPoint) {
-  let shorts = await gtfsTimetables.findDocuments({
+  let changeoverStop = await stops.findDocument({
+    stopName: changeoverPoint
+  })
+
+  let stopGTFSIDs = changeoverStop.bays.filter(e => e.mode.includes('regional')).map(e => e.stopGTFSID)
+
+  let trips = await gtfsTimetables.findDocuments({
     mode,
-    'stopTimings.stopName': changeoverPoint
+    'stopTimings.stopGTFSID': {
+      $in: stopGTFSIDs
+    }
   }).toArray()
 
   let connectionsMade = 0
 
-  await async.forEachLimit(shorts, 50, async trip => {
-    let stop = trip.stopTimings.find(stop => stop.stopName === changeoverPoint)
-    let validTimes = []
-    for (let i = 1; i <= 15; i++) {
-      validTimes.push(stop.arrivalTimeMinutes + i)
+  await async.forEachLimit(trips, 50, async trip => {
+    let stop = trip.stopTimings.find(stop => stopGTFSIDs.includes(stop.stopGTFSID))
+
+    if (!stop.arrivalTimeMinutes) return
+    validTimes = {
+      $gte: stop.arrivalTimeMinutes + 1,
+      $lte: stop.arrivalTimeMinutes + 30
     }
 
     for (let operationDay of trip.operationDays) {
-      let connection = await gtfsTimetables.findDocument({
+      let connections = await gtfsTimetables.findDocuments({
         mode,
-        operationDays: operationDay,
-        stopTimings: {
-          $elemMatch: {
-            stopName: changeoverPoint,
-            $or: [{
-              departureTimeMinutes: {
-                $in: validTimes
-              }
-            }, {
-              arrivalTimeMinutes: {
-                $in: validTimes
-              }
-            }]
+        routeGTFSID: {
+          $not: {
+            $eq: trip.routeGTFSID
           }
         },
-        destination: {
+        operationDays: operationDay,
+        tripID: {
           $not: {
-            $eq: trip.destination
+            $eq: trip.tripID
+          }
+        },
+        stopTimings: {
+          $elemMatch: {
+            stopGTFSID: {
+              $in: stopGTFSIDs
+            },
+            $or: [{
+              departureTimeMinutes: validTimes
+            }, {
+              arrivalTimeMinutes: validTimes
+            }],
+            'stopConditions.pickup': 0
           }
         }
-      })
+      }).toArray()
 
-      if (connection) {
+      await async.forEach(connections, async connection => {
+        let lastStop = connection.stopTimings.slice(-1)[0].stopGTFSID
+        if (stopGTFSIDs.includes(lastStop)) return
+
+        if (trip.mode === 'regional train' && connection.mode === 'regional train') {
+          if (trip.direction !== connection.direction) return
+        }
+
+        if (trip.origin.includes('Southern Cross') && connection.destination.includes('Southern Cross')) return
+        if (trip.destination.includes('Southern Cross') && connection.destination.includes('Southern Cross')) return
+        if (trip.origin.includes('Southern Cross') && connection.origin.includes('Southern Cross')) return
+
         connectionsMade++
         if (!trip.connections) trip.connections = []
 
-        if (!trip.connections.find(c => c.tripID === connection.tripID)) {
+        let match = trip.connections.find(c => c.tripID === connection.tripID)
+        if (match) {
+          if (!match.operationDays.includes(operationDay)) {
+            match.operationDays.push(operationDay)
+          }
+        } else {
           trip.connections.push({
-            operationDay,
+            operationDays: [operationDay],
             changeAt: changeoverPoint,
             for: connection.destination,
-            from: trip.destination,
             tripID: connection.tripID
           })
-
-          await gtfsTimetables.updateDocument({
-            _id: trip._id
-          }, {
-            $set: {
-              connections: trip.connections
-            }
-          })
         }
-      }
+
+        await gtfsTimetables.updateDocument({
+          _id: trip._id
+        }, {
+          $set: {
+            connections: trip.connections
+          }
+        })
+      })
     }
   })
 
@@ -86,10 +115,33 @@ database.connect({
   poolSize: 100
 }, async err => {
   gtfsTimetables = database.getCollection('gtfs timetables')
+  stops = database.getCollection('stops')
 
   let count = 0
 
   count += await findConnections('Koo Wee Rup Bus Interchange/Rossiter Road')
+  count += await findConnections('Ballarat Railway Station')
+  count += await findConnections('Geelong Railway Station')
+  count += await findConnections('Warrnambool Railway Station')
+  count += await findConnections('Ararat Railway Station')
+  count += await findConnections('Stawell Railway Station')
+  count += await findConnections('Car Park Rear Toilet Block/Lloyd Street') // Dimboola
+  count += await findConnections('Castlemaine Railway Station')
+  count += await findConnections('Swan Hill Railway Station')
+  count += await findConnections('Bendigo Railway Station')
+  count += await findConnections('Rochester Railway Station')
+  count += await findConnections('Jennings Street/Northern Highway') // Heathcote
+  count += await findConnections('Shepparton Railway Station')
+  count += await findConnections('Wangaratta Railway Station')
+  count += await findConnections('Woodend Railway Station')
+  count += await findConnections('Kerang Railway Station')
+  count += await findConnections('Murchison East Railway Station')
+  count += await findConnections('Seymour Railway Station')
+  count += await findConnections('Wodonga Railway Station')
+  count += await findConnections('Benalla Railway Station')
+  count += await findConnections('Traralgon Railway Station')
+  count += await findConnections('Sale Railway Station')
+  count += await findConnections('Bairnsdale Railway Station')
 
   updateStats('vline-connections', count)
   console.log('Completed loading in ' + count + ' vline connections')
