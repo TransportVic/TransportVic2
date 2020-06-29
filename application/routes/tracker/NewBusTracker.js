78be3cf0f71644e4579e0137465b5160a8d7d351
Eyeball Edward <edwardyeung.private@gmail.com>
2020-06-29 19:15:47 +1000

    track by service in too

--- application/routes/tracker/NewBusTracker.js
+++ application/routes/tracker/NewBusTracker.js
@@ -45,8 +45,18 @@ router.get('/fleet', async (req, res) => {
   let today = utils.getYYYYMMDDNow()
 
   let {fleet, date} = querystring.parse(url.parse(req.url).query)
-  if (!fleet) return res.end()
+
   if (!date) date = today
+  if (!fleet) {
+    return res.render('tracker/bus/by-fleet', {
+      tripsToday: [],
+      servicesByDay: {},
+      bus: null,
+      fleet: '?',
+      smartrakID: '?',
+      date: moment(date, 'YYYYMMDD')
+    })
+  }
 
   let bus = await smartrakIDs.findDocument({ fleetNumber: fleet })
   let query = { date }
@@ -61,8 +71,6 @@ router.get('/fleet', async (req, res) => {
     bus = await smartrakIDs.findDocument({ smartrakID })
     if (bus) {
       fleet = `#${bus.fleetNumber}`
-    } else {
-      fleet = '?'
     }
   }
 
@@ -101,6 +109,86 @@ router.get('/fleet', async (req, res) => {
     bus,
     fleet,
     smartrakID,
+    date: moment(date, 'YYYYMMDD')
+  })
+})
+
+
+
+router.get('/service', async (req, res) => {
+  let {db} = res
+  let busTrips = db.getCollection('bus trips')
+  let smartrakIDs = db.getCollection('smartrak ids')
+
+  let minutesPastMidnightNow = utils.getMinutesPastMidnightNow()
+
+  let today = utils.getYYYYMMDDNow()
+
+  let {service, date} = querystring.parse(url.parse(req.url).query)
+
+  if (!date) date = today
+  if (!service) {
+    return res.render('tracker/bus/by-service', {
+      tripsToday: [],
+      busesByDay: {},
+      service: '',
+      date: moment(date, 'YYYYMMDD')
+    })
+  }
+
+  let rawTripsToday = await busTrips.findDocuments({
+    date,
+    routeNumber: service
+  }).sort({departureTime: 1, origin: 1}).toArray()
+
+  let tripsToday = (await async.map(rawTripsToday, async rawTrip => {
+    let {fleetNumber} = await smartrakIDs.findDocument({
+      smartrakID: rawTrip.smartrakID
+    }) || {}
+
+    let {origin, destination, departureTime, destinationArrivalTime} = rawTrip
+
+    let departureTimeMinutes = utils.time24ToMinAftMidnight(departureTime),
+        destinationArrivalTimeMinutes = utils.time24ToMinAftMidnight(destinationArrivalTime)
+
+    rawTrip.active = minutesPastMidnightNow <= destinationArrivalTimeMinutes || date !== today
+    rawTrip.fleetNumber = fleetNumber ? '#' + fleetNumber : '@' + rawTrip.smartrakID
+    return rawTrip
+  })).map(adjustTrip)
+
+  let operationDays = await busTrips.distinct('date', {
+    routeNumber: service
+  })
+  let busesByDay = {}
+  let smartrakIDCache = {}
+
+  await async.forEach(operationDays, async date => {
+    let smartrakIDsByDate = await busTrips.distinct('smartrakID', {
+      date,
+      routeNumber: service
+    })
+    let buses = (await async.map(smartrakIDsByDate, async smartrakID => {
+      let fleetNumber = smartrakIDCache[smartrakID]
+      if (!fleetNumber) {
+        let lookup = await smartrakIDs.findDocument({
+          smartrakID
+        })
+        if (lookup) {
+          fleetNumber = lookup.fleetNumber
+          smartrakIDCache[smartrakID] = fleetNumber
+        }
+      }
+      return fleetNumber ? '#' + fleetNumber : '@' + smartrakID
+    })).sort((a, b) => a.replace(/[^\d]/g, '') - b.replace(/[^\d]/g, ''))
+
+    let humanDate = date.slice(6, 8) + '/' + date.slice(4, 6) + '/' + date.slice(0, 4)
+    busesByDay[humanDate] = buses
+  })
+
+  res.render('tracker/bus/by-service', {
+    tripsToday,
+    busesByDay,
+    service,
     date: moment(date, 'YYYYMMDD')
   })
 })
