cc8993e47cbae0f3227f1656f38953d8d7db4b97
Eyeball Edward <edwardyeung.private@gmail.com>
2020-07-05 15:17:38 +1000

    always mark CCL as down

--- modules/utils/get-stopping-pattern.js
+++ modules/utils/get-stopping-pattern.js
@@ -145,6 +145,7 @@ module.exports = async function (db, ptvRunID, mode, time, stopID, referenceTrip
   let direction
   if (mode === 'metro train') {
     direction = ptvDirection.direction_id === 1 ? 'Up' : 'Down'
+    if (routeGTFSID === '2-CCL') direction = 'Down'
   }
 
   let routeName = routeData.route_name.trim()
