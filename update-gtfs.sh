37dfea18ace31df197f4b524f4a0b7de01b779b4
Eyeball Edward <edwardyeung.private@gmail.com>
2020-07-18 11:07:00 +1000

    derp

--- update-gtfs.sh
+++ update-gtfs.sh
@@ -2,3 +2,14 @@ cd gtfs
 rm -r *
 curl http://data.ptv.vic.gov.au/downloads/gtfs.zip --output gtfs.zip
 unzip gtfs.zip
+for i in {1..8}; do
+  cd $i
+  unzip google_transit.zip
+  cd ..
+done
+
+for i in {10..11}; do
+  cd $i
+  unzip google_transit.zip
+  cd ..
+done
