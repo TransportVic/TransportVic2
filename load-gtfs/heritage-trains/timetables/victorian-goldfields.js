b528d43789deed926176197ef155959f80aff59d
Eyeball Edward <edwardyeung.private@gmail.com>
2020-07-26 17:49:07 +1000

    victorian goldfields

---
+++ load-gtfs/heritage-trains/timetables/victorian-goldfields.js
@@ -0,0 +1,146 @@
+module.exports = {
+  "days": [
+    ["WED_SUN", "0", "0", "1", "0", "0", "0", "1", "20200101", "20201231"]
+  ],
+  "dates": [],
+  "trips": [
+    {
+      "mode": "heritage train",
+      "routeGTFSID": "13-VGR",
+      "calendarID": "WED_SUN",
+      "tripID": "1030.WED_SUN.13-VGR-mjp-1.1.R",
+      "gtfsDirection": "1",
+      "shapeID": "13-VGR-mjp-1.1.R",
+      "headsign": "Castlemaine"
+    },
+    {
+      "mode": "heritage train",
+      "routeGTFSID": "13-VGR",
+      "calendarID": "WED_SUN",
+      "tripID": "1200.WED_SUN.13-VGR-mjp-1.1.H",
+      "gtfsDirection": "0",
+      "shapeID": "13-VGR-mjp-1.1.H",
+      "headsign": "Maldon"
+    },
+    {
+      "mode": "heritage train",
+      "routeGTFSID": "13-VGR",
+      "calendarID": "WED_SUN",
+      "tripID": "1445.WED_SUN.13-VGR-mjp-1.1.R",
+      "gtfsDirection": "1",
+      "shapeID": "13-VGR-mjp-1.1.R",
+      "headsign": "Castlemaine"
+    },
+    {
+      "mode": "heritage train",
+      "routeGTFSID": "13-VGR",
+      "calendarID": "WED_SUN",
+      "tripID": "1600.WED_SUN.13-VGR-mjp-1.1.H",
+      "gtfsDirection": "0",
+      "shapeID": "13-VGR-mjp-1.1.H",
+      "headsign": "Maldon"
+    }
+  ],
+  "timings": [
+    {
+      "tripID": "1030.WED_SUN.13-VGR-mjp-1.1.R",
+      "stopTimings": [
+        {
+          "stopGTFSID": 13000011,
+          "arrivalTime": "10:30:00",
+          "departureTime": "10:30:00",
+          "stopConditions": {
+            "dropoff": 0, "pickup": 0
+          },
+          "stopDistance": 0,
+          "stopSequence": 0
+        },
+        {
+          "stopGTFSID": 13000012,
+          "arrivalTime": "11:15:00",
+          "departureTime": "11:15:00",
+          "stopConditions": {
+            "dropoff": 0, "pickup": 0
+          },
+          "stopDistance": 0,
+          "stopSequence": 0
+        }
+      ]
+    },
+    {
+      "tripID": "1200.WED_SUN.13-VGR-mjp-1.1.H",
+      "stopTimings": [
+        {
+          "stopGTFSID": 13000012,
+          "arrivalTime": "12:00:00",
+          "departureTime": "12:00:00",
+          "stopConditions": {
+            "dropoff": 0, "pickup": 0
+          },
+          "stopDistance": 0,
+          "stopSequence": 0
+        },
+        {
+          "stopGTFSID": 13000011,
+          "arrivalTime": "12:45:00",
+          "departureTime": "12:45:00",
+          "stopConditions": {
+            "dropoff": 0, "pickup": 0
+          },
+          "stopDistance": 0,
+          "stopSequence": 0
+        }
+      ]
+    },
+    {
+      "tripID": "1445.WED_SUN.13-VGR-mjp-1.1.R",
+      "stopTimings": [
+        {
+          "stopGTFSID": 13000011,
+          "arrivalTime": "14:45:00",
+          "departureTime": "14:45:00",
+          "stopConditions": {
+            "dropoff": 0, "pickup": 0
+          },
+          "stopDistance": 0,
+          "stopSequence": 0
+        },
+        {
+          "stopGTFSID": 13000012,
+          "arrivalTime": "15:30:00",
+          "departureTime": "15:30:00",
+          "stopConditions": {
+            "dropoff": 0, "pickup": 0
+          },
+          "stopDistance": 0,
+          "stopSequence": 0
+        }
+      ]
+    },
+    {
+      "tripID": "1600.WED_SUN.13-VGR-mjp-1.1.H",
+      "stopTimings": [
+        {
+          "stopGTFSID": 13000012,
+          "arrivalTime": "16:00:00",
+          "departureTime": "16:00:00",
+          "stopConditions": {
+            "dropoff": 0, "pickup": 0
+          },
+          "stopDistance": 0,
+          "stopSequence": 0
+        },
+        {
+          "stopGTFSID": 13000011,
+          "arrivalTime": "16:45:00",
+          "departureTime": "16:45:00",
+          "stopConditions": {
+            "dropoff": 0, "pickup": 0
+          },
+          "stopDistance": 0,
+          "stopSequence": 0
+        }
+      ]
+    }
+  ]
+}
