0d8f18fb3369179829be437ad5863e05a3a583d6
Eyeball Edward <edwardyeung.private@gmail.com>
2020-09-11 12:38:45 +1000

    vline highlights

--- application/routes/tracker/VLineTracker.js
+++ application/routes/tracker/VLineTracker.js
@@ -109,4 +109,55 @@ router.get('/consist', async (req, res) => {
   })
 })
 
+router.get('/highlights', async (req, res) => {
+  let {db} = res
+  let vlineTrips = db.getCollection('vline trips')
+  let nspTimetables = db.getCollection('timetables')
+  let today = utils.getYYYYMMDDNow()
+  let {consist, date} = querystring.parse(url.parse(req.url).query)
+  if (date) date = utils.getYYYYMMDD(utils.parseDate(date))
+  else date = today
+
+  let minutesPastMidnightNow = utils.getMinutesPastMidnightNow()
+
+  let allTrips = (await vlineTrips.findDocuments({ date })
+    .sort({departureTime: 1, destination: 1}).toArray())
+    .map(trip => adjustTrip(trip, date, today, minutesPastMidnightNow))
+
+  let doubleHeaders = allTrips.filter(trip => {
+    return trip.consist[0].startsWith('N') && trip.consist[1].startsWith('N')
+  })
+
+  let consistTypeChanged = await async.filter(allTrips, async trip => {
+    let nspTimetable = await nspTimetables.findDocument({ runID: trip.runID })
+    if (nspTimetable) {
+      let tripVehicleType
+      if (trip.consist[0].startsWith('VL')) tripVehicleType = 'VL'
+      else if (trip.consist[0].startsWith('70')) tripVehicleType = 'SP'
+      else {
+        tripVehicleType = 'N +'
+        let carriage = trip.consist.find(c => !c.startsWith('N') && !c.startsWith('P'))
+        tripVehicleType += carriage.includes('N') ? 'N' : 'H'
+      }
+
+      let nspTripType = nspTimetable.vehicle
+      let vlMatch = (nspTripType.includes('VL') || nspTripType.includes('VR')) && tripVehicleType == 'VL'
+      let spMatch = nspTripType.includes('SP') && tripVehicleType == 'SP'
+
+      if (vlMatch || spMatch) return false
+      if (nspTripType.startsWith('N') && tripVehicleType.startsWith('N')) {
+        return tripVehicleType.slice(-1) !== nspTripType.slice(-1)
+      }
+
+      return true
+    }
+  })
+
+  res.render('tracker/vline/highlights', {
+    doubleHeaders,
+    consistTypeChanged,
+    date: utils.parseTime(date, 'YYYYMMDD')
+  })
+})
+
 module.exports = router
