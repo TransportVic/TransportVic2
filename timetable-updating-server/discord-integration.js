a5006f44f1bcebc0f66c0f4d124893db44661e27
Eyeball Edward <edwardyeung.private@gmail.com>
2020-09-27 13:54:43 +1000

    cleanup discord integration

--- timetable-updating-server/discord-integration.js
+++ timetable-updating-server/discord-integration.js
@@ -3,8 +3,8 @@ const config = require('../config.json')
 const modules = require('../modules.json')
 
 module.exports = async text => {
-  if (modules.updaterDiscordIntegration) {
-    await utils.request(config.discordUpdaterURL, {
+  if (modules.discordIntegration && modules.discordIntegration.timetables) {
+    await utils.request(config.discordURLs.timetables, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
