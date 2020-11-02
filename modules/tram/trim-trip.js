419b2fd89aadd5aa3ed3516e312f085faad0b678
Eyeball Edward <edwardyeung.private@gmail.com>
2020-11-02 19:43:30 +1100

    force abbotsford st int

--- modules/tram/trim-trip.js
+++ modules/tram/trim-trip.js
@@ -60,6 +60,7 @@ module.exports.trimFromDestination = async function(db, destination, coreRoute, 
   let cutoffStop
   let stops = db.getCollection('stops')
 
+  if (destination === 'Abbotsford St Intg') destination = 'Royal Childrens Hospital'
   let selectionMethod = destination.includes('&')
   let baseDestination = destination.replace(/&.*/, '').trim()
 
@@ -70,7 +71,7 @@ module.exports.trimFromDestination = async function(db, destination, coreRoute, 
       let stopData = await getStopData(stop.stopGTFSID, stops)
       if (stopData.tramTrackerNames) {
         let matched = selectionMethod ? stopData.tramTrackerNames.some(name => name.includes(baseDestination))
-          : stopData.tramTrackerNames.includes(baseDestination)
+          : stopData.tramTrackerNames.find(name => distance(name, destination) <= 3)
         if (matched) {
           cutoffStop = stop.stopGTFSID
         }
