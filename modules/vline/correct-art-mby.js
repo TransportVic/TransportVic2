3f3a3074263c59505997ad3953da84615744102a
Eyeball Edward <edwardyeung.private@gmail.com>
2020-11-07 08:58:03 +1100

    fix

--- modules/vline/correct-art-mby.js
+++ modules/vline/correct-art-mby.js
@@ -2,10 +2,13 @@ const utils = require('../../utils')
 const findTrip = require('./find-trip')
 
 module.exports = async function(departure, trip, gtfsTimetables, departureDay) {
-  let tripDestinationArrivalHHMM = trip.destinationArrivalTime
-  let minutesPastMidnight = utils.getMinutesPastMidnightFromHHMM(tripDestinationArrivalHHMM)
-  let destinationArrivalTime = utils.getMomentFromMinutesPastMidnight(minutesPastMidnight, utils.now())
-  // We can take the shortcut approach of MPM, now as these trips do not run at odd hours of the day
+  let destinationArrivalTime
+  if (trip) {
+    let tripDestinationArrivalHHMM = trip.destinationArrivalTime
+    let minutesPastMidnight = utils.getMinutesPastMidnightFromHHMM(tripDestinationArrivalHHMM)
+    let destinationArrivalTime = utils.getMomentFromMinutesPastMidnight(minutesPastMidnight, utils.now())
+    // We can take the shortcut approach of MPM, now as these trips do not run at odd hours of the day
+  }
 
   if (departure.origin === 'Maryborough Railway Station' && (departure.runID === '8118' || departure.runID === '8158')) { // Maryborough - Southern Cross, joins with the Ararat - Ballarat
     trip = trip || await findTrip(gtfsTimetables, departureDay, departure.origin, 'Ballarat Railway Station', utils.formatHHMM(departure.originDepartureTime))
