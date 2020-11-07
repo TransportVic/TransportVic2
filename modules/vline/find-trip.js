4e8b17ae88b27512f5ece204e47848cf2b830458
Eyeball Edward <edwardyeung.private@gmail.com>
2020-11-07 19:30:37 +1100

    up to 13min variance for long distance trains

--- modules/vline/find-trip.js
+++ modules/vline/find-trip.js
@@ -1,12 +1,57 @@
 const utils = require('../../utils')
 
+let longDistanceCountryStops = [
+  "Albury",
+  "Ararat",
+  "Avenel",
+  "Bairnsdale",
+  "Beaufort",
+  "Benalla",
+  "Birregurra",
+  "Broadford",
+  "Camperdown",
+  "Chiltern",
+  "Colac",
+  "Dingee",
+  "Echuca",
+  "Euroa",
+  "Kerang",
+  "Mooroopna",
+  "Murchison East",
+  "Nagambie",
+  "Pyramid",
+  "Rochester",
+  "Rosedale",
+  "Sale",
+  "Shepparton",
+  "Springhurst",
+  "Stratford",
+  "Swan Hill",
+  "Terang",
+  "Violet Town",
+  "Wangaratta",
+  "Warrnambool",
+  "Winchelsea",
+  "Sherwood Park",
+  "Creswick",
+  "Clunes",
+  "Maryborough",
+  "Talbot",
+  "Epsom"
+].map(x => x + ' Railway Station')
+
 module.exports = async (collection, operationDay, origin, destination, departureTime) => {
   let tripStartMinutes = utils.getMinutesPastMidnightFromHHMM(departureTime)
   if (tripStartMinutes < 180) tripStartMinutes += 1440
 
+  let varianceAllowed = 5
+  if (longDistanceCountryStops.includes(origin) || longDistanceCountryStops.includes(destination)) {
+    varianceAllowed = 13
+  }
+
   let variedDepartureTime = {
-    $gte: tripStartMinutes - 5,
-    $lte: tripStartMinutes + 5
+    $gte: tripStartMinutes - varianceAllowed,
+    $lte: tripStartMinutes + varianceAllowed
   }
 
   let query = {
