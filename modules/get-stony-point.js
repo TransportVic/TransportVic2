92bbb4ddbbfc1e568428a37e7c9f48c072d3d542
Eyeball Edward <edwardyeung.private@gmail.com>
2020-11-12 21:51:05 +1100

    refine sprinter check

--- modules/get-stony-point.js
+++ modules/get-stony-point.js
@@ -1,25 +1,78 @@
 const utils = require('../utils')
 
-let allSprinters = []
-for (let i = 7001; i <= 7022; i++) {
-  if (i !== 7019 && i !== 7015) allSprinters.push(i.toString())
+function getPreviousDay(day, dayOfWeek) {
+  for (let i = 0; i <= 7; i++) {
+    let checkDay = day.clone().add(-i, 'days')
+    if (utils.getDayOfWeek(checkDay) === dayOfWeek) return checkDay
+  }
 }
 
-module.exports = async db => {
-  let vlineTrips = db.getCollection('vline trips')
-  let now = utils.now()
-  let prevFriday = utils.now()
-  prevFriday.day(prevFriday.day() >= 5 ? 5 : -2)
+async function getMissingTrains(onDay, untilDay, vlineTrips, allSprinters) {
+  let days = utils.allDaysBetweenDates(untilDay, onDay).map(d => utils.getYYYYMMDD(d))
 
-  let days = utils.allDaysBetweenDates(prevFriday, now).map(d => utils.getYYYYMMDD(d))
-
-  let sprinters = (await vlineTrips.distinct('consist', {
+  let sprinters = await vlineTrips.distinct('consist', {
     consist: /70\d\d/,
     date: {
       $in: days
     }
+  })
+
+  return allSprinters.filter(s => !sprinters.includes(s))
+}
+
+function getRotationStart(fromDay) {
+  let currentDay = utils.getDayOfWeek(fromDay)
+
+  if (!utils.isWeekday(currentDay) || currentDay === 'Fri') {
+    return getPreviousDay(fromDay, 'Fri')
+  } else {
+    return getPreviousDay(fromDay, 'Mon')
+  }
+}
+
+module.exports = async db => {
+  let vlineTrips = db.getCollection('vline trips')
+
+  let now = utils.now()
+  let recentSprinters = (await vlineTrips.distinct('consist', {
+    consist: /70\d\d/,
+    date: {
+      $in: utils.allDaysBetweenDates(now.clone().add(-14, 'days'), now)
+        .map(d => utils.getYYYYMMDD(d))
+    }
   })).sort((a, b) => a - b)
 
-  let missing = allSprinters.filter(s => !sprinters.includes(s))
-  return missing
+  let untilDay = getRotationStart(now)
+  let previousRotationStart = untilDay.clone()
+
+  let previous2Missing = []
+  for (let i = 0; i < 2; i++) {
+    let previousRotationEnd = previousRotationStart.clone().add(-1, 'day')
+    previousRotationStart = getRotationStart(previousRotationEnd)
+
+    previous2Missing[i] = await getMissingTrains(previousRotationEnd, previousRotationStart, vlineTrips, recentSprinters)
+  }
+
+  let missingBoth = previous2Missing[0].filter(train => previous2Missing[1].includes(train))
+  let currentRotationMissing = await getMissingTrains(now, untilDay, vlineTrips, recentSprinters)
+
+  let potential = currentRotationMissing.filter(train => !missingBoth.includes(train) && !previous2Missing[0].includes(train))
+
+  if (potential.length === 2) return potential
+  else if (potential.length > 2) {
+    let previousRotationEnd = untilDay.clone().add(-1, 'day')
+
+    for (let check of potential) {
+      let pairedWith = await vlineTrips.distinct('consist', {
+        consist: check,
+        date: utils.getYYYYMMDD(previousRotationEnd)
+      })
+
+      if (pairedWith.length === 2 && pairedWith.some(train => potential.includes(train))) {
+        return pairedWith
+      }
+    }
+  }
+
+  return potential
 }
