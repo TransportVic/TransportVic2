13326a6e59bb3bfdfb27a32b8e4a52577403a1db
Eyeball Edward <edwardyeung.private@gmail.com>
2020-11-13 16:47:10 +1100

    STY directions

--- modules/utils/get-stopping-pattern.js
+++ modules/utils/get-stopping-pattern.js
@@ -218,6 +218,11 @@ module.exports = async function (db, ptvRunID, mode, time, stopID, referenceTrip
   let direction
   if (mode === 'metro train') {
     direction = ptvDirection.direction_id === 0 ? 'Up' : 'Down'
+    if (routeGTFSID === '2-SPT') {
+      let origin = stopTimings[0].stopName
+      if (origin === 'Frankston Railway Station') direction = 'Down'
+      else direction = 'Up'
+    }
     if (routeGTFSID === '2-CCL') direction = 'Down'
   }
 
