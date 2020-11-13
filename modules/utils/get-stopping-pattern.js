3618b897d53368221b5ed22712b11200218dce3e
Eyeball Edward <edwardyeung.private@gmail.com>
2020-11-13 16:37:33 +1100

    metro UP is now direction 0

--- modules/utils/get-stopping-pattern.js
+++ modules/utils/get-stopping-pattern.js
@@ -217,7 +217,7 @@ module.exports = async function (db, ptvRunID, mode, time, stopID, referenceTrip
 
   let direction
   if (mode === 'metro train') {
-    direction = ptvDirection.direction_id === 1 ? 'Up' : 'Down'
+    direction = ptvDirection.direction_id === 0 ? 'Up' : 'Down'
     if (routeGTFSID === '2-CCL') direction = 'Down'
   }
 
