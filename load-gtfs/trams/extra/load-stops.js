b327c3fa49ab5e9519f4a72028380271ba3b96c3
Eyeball Edward <edwardyeung.private@gmail.com>
2020-11-13 14:10:21 +1100

    tramtracker fixes

--- load-gtfs/trams/extra/load-stops.js
+++ load-gtfs/trams/extra/load-stops.js
@@ -1,18 +1,26 @@
+const utils = require('../../../utils')
+const ptvAPI = require('../../../ptv-api')
+const fs = require('fs')
 
+ptvAPI.getPTVKey().then(async key => {
+  let data = JSON.parse(await utils.request('https://www.ptv.vic.gov.au/lithe/stored-stops-all?__tok=' + key))
 
-require('fs').writeFileSync(__dirname + '/tram-stops.json', JSON.stringify(require('./stops').stops.filter(x=>x.primaryChronosMode === '1').map(x=>{
-  let m = x.title.match(/^(D?\d+[a-zA-Z]?)-/)
-  let stopName = x.title.split(' #')[0]
-  let stopNumber = x.title.split(' #')[1]
+  fs.writeFileSync(__dirname + '/tram-stops.json', JSON.stringify(data.stops.filter(x => x.primaryChronosMode === '1').map(x => {
+    let m = x.title.match(/^(D?\d+[a-zA-Z]?)-/)
+    let stopName = x.title.split(' #')[0]
+    let stopNumber = x.title.split(' #')[1]
 
-  if (m) {
-    stopNumber = m[1]
-    stopName = stopName.replace(m[0], '')
-  }
+    if (m) {
+      stopNumber = m[1]
+      stopName = stopName.replace(m[0], '')
+    }
 
-  stopName = stopNumber + '-' + stopName
+    stopName = stopNumber + '-' + stopName
 
-  return {
-    stopName, stopNumber, stopID: x.id
-  }
-})))
+    return {
+      stopName, stopNumber, stopID: x.id
+    }
+  })))
+
+  process.exit()
+})
