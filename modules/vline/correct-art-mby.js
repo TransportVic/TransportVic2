617939a8ed66ebae41718b9000ad1389a67f8936
Eyeball Edward <edwardyeung.private@gmail.com>
2020-11-30 08:39:16 +1100

    fix ART MBY swap

--- modules/vline/correct-art-mby.js
+++ modules/vline/correct-art-mby.js
@@ -12,18 +12,18 @@ function getDestinationArrivalTime(trip) {
 
 module.exports = async function(departure, trip, gtfsTimetables, departureDay) {
   try {
-    if (departure.origin === 'Maryborough Railway Station' && (departure.runID === '8118' || departure.runID === '8158')) { // Maryborough - Southern Cross, joins with the Ararat - Ballarat
+    if (departure.runID === '8118' || departure.runID === '8158') { // Maryborough - Southern Cross, joins with the Ararat - Ballarat
       trip = trip || await findTrip(gtfsTimetables, departureDay, departure.origin, 'Ballarat Railway Station', utils.formatHHMM(departure.originDepartureTime))
-      if (trip) {
+      if (trip && trip.origin === 'Maryborough Railway Station') {
         departure.destination = 'Ballarat Railway Station'
         departure.destinationArrivalTime = getDestinationArrivalTime(trip)
       }
     }
 
-    if (departure.origin === 'Ararat Railway Station' && (departure.runID === '8116' || departure.runID === '8160')) { // Ararat - Ballarat, but PTV programs it as Ararat - Southern Cross so we extend the tracker data
+    if (departure.runID === '8116' || departure.runID === '8160') { // Ararat - Ballarat, but PTV programs it as Ararat - Southern Cross so we extend the tracker data
       // Match up to BAL as disruptions could mean this service might not actually run all the way to SSS
       trip = trip || await findTrip(gtfsTimetables, departureDay, departure.origin, 'Ballarat Railway Station', utils.formatHHMM(departure.originDepartureTime))
-      if (trip) {
+      if (trip && trip.origin === 'Ararat Railway Station') {
         departure.destination = trip.destination
         departure.destinationArrivalTime = getDestinationArrivalTime(trip)
       }
