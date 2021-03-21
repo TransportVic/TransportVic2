d541bf708d724723024a4dfcd314915e82288060
Eyeball Edward <edwardyeung.private@gmail.com>
2021-03-21 21:34:52 +1100

    daylight saving update

--- update-calendars.sh
+++ update-calendars.sh
@@ -3,4 +3,4 @@ DIRNAME=$(dirname "$0")
 
 cd "$DIRNAME/additional-data" || return
 curl https://www.vic.gov.au/sites/default/files/2020-09/Victorian-public-holiday-dates.ics --output vic-holidays.ics
-curl https://www.vic.gov.au/sites/default/files/2019-10/Daylight-saving.ics --output daylight-saving.ics
+curl https://www.vic.gov.au/sites/default/files/2021-03/Daylight-saving.ics --output daylight-saving.ics
