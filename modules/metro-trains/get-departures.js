ebece664abf8575a3ce4ab1f14a77f2eff79aaf5
Eyeball Edward <edwardyeung.private@gmail.com>
2021-05-17 06:49:53 +1000

    expand STY bus stuff

--- modules/metro-trains/get-departures.js
+++ modules/metro-trains/get-departures.js
@@ -176,7 +176,7 @@ async function getNotifyData(db) {
   let allTrainAlterations = disruptions.filter(disruption => disruption.runID)
   let stonyPointReplacements = allTrainAlterations.filter(disruption => {
     let text = disruption.text
-    return disruption.routeName === 'Stony Point' && (text.includes('replace') || text.includes('coach'))
+    return disruption.routeName === 'Stony Point' && (text.includes('replace') || text.includes('coach') || text.includes('bus'))
   }).map(disruption => disruption.runID)
 
   let loopSkipping = allTrainAlterations.filter(disruption => {
