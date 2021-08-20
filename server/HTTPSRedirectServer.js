78be6ad0d4e3b4e55d4da311eeec6e3e38d24ee0
Eyeball Edward <edwardyeung39@gmail.com>
2021-08-20 21:32:44 +1000

    stuff

--- server/HTTPSRedirectServer.js
+++ server/HTTPSRedirectServer.js
@@ -4,7 +4,9 @@ const fs = require('fs')
 const config = require('../config.json')
 
 module.exports = class HTTPSRedirectServer {
-  app (req, res) {
+  app (req, res, next) {
+    if (req.url.startsWith('.well-known')) return next()
+
     const redirectedURL = 'https://' + req.headers.host + req.url
 
     res.writeHead(308, { Location: redirectedURL })
