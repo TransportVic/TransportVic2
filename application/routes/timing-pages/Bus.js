49e09437f82717c717b7b3b402b624ec6bfea82b
Eyeball Edward <edwardyeung39@gmail.com>
2021-08-20 15:49:05 +1000

    Revert todays code as transdev announced services back to normal
    This reverts commit 5ebcb6702c64a610c9b83c872d3a33789ab89dc9.

--- application/routes/timing-pages/Bus.js
+++ application/routes/timing-pages/Bus.js
@@ -30,8 +30,6 @@ async function loadDepartures(req, res) {
       late: 5
     })
 
-    if (departure.cancelled) departure.headwayDevianceClass = 'cancelled'
-
     departure.codedLineName = utils.encodeName(departure.trip.routeName)
 
     let currentStop = departure.trip.stopTimings.find(tripStop => stopGTFSIDs.includes(tripStop.stopGTFSID))
