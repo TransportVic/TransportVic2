1c20a8863b9ac45a1c6075a671aadc7751999f84
Eyeball Edward <edwardyeung.private@gmail.com>
2022-03-19 15:55:22 +0800

    reflect service rename

--- start-maintenance.sh
+++ start-maintenance.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 DIRNAME=$(dirname "$0")
 
-sudo systemctl stop server
+sudo systemctl stop transportvic
 sudo systemctl restart mongod
 node "$DIRNAME/timetable-updating-server/index.js" | tee "$DIRNAME/updater-log.txt"
 sudo systemctl restart mongod
-sudo systemctl start server
+sudo systemctl start transportvic
