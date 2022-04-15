53b56b769de49ba69018f4dd4e8e0d46edeb9ff0
Eyeball Edward <edwardyeung39@gmail.com>
2022-04-15 13:49:13 +0800

    make generation of bus route groupings automatic

--- load-gtfs/bus/load-all.sh
+++ load-gtfs/bus/load-all.sh
@@ -7,3 +7,4 @@ node "$DIRNAME/extra/load-788-stop-numbers.js"
 #"$DIRNAME/load-type.sh" 7   # Telebus replaced by Flexiride
 #"$DIRNAME/load-type.sh" 8   # Night bus moved to 4- routes
 "$DIRNAME/load-type.sh" 11
+node "$DIRNAME/../../additional-data/bus-data/geospatial/generate-bus-groupings.js"
