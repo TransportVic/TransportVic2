a93db45e97203efa9a23e94adb22597295c95233
Eyeball Edward <edwardyeung.private@gmail.com>
2022-04-15 19:57:33 +0800

    make backup script non user dependent

--- backup.sh
+++ backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+DIRNAME=$(dirname "$0")
 
-mongodump --collection="smartrak ids" --db="TransportVic" --gzip --archive="/home/ec2-user/backup/smartrak-ids.bson.gz"
-mongodump --collection="bus trips" --db="TransportVic" --gzip --archive="/home/ec2-user/backup/bus-trips.bson.gz"
-mongodump --collection="tram trips" --db="TransportVic" --gzip --archive="/home/ec2-user/backup/tram-trips.bson.gz"
-mongodump --collection="metro trips" --db="TransportVic" --gzip --archive="/home/ec2-user/backup/metro-trips.bson.gz"
-mongodump --collection="vline trips" --db="TransportVic" --gzip --archive="/home/ec2-user/backup/vline-trips.bson.gz"
+mongodump --collection="smartrak ids" --db="TransportVic" --gzip --archive="$DIRNAME/backup/smartrak-ids.bson.gz"
+mongodump --collection="bus trips" --db="TransportVic" --gzip --archive="$DIRNAME/backup/bus-trips.bson.gz"
+mongodump --collection="tram trips" --db="TransportVic" --gzip --archive="$DIRNAME/backup/tram-trips.bson.gz"
+mongodump --collection="metro trips" --db="TransportVic" --gzip --archive="$DIRNAME/backup/metro-trips.bson.gz"
+mongodump --collection="vline trips" --db="TransportVic" --gzip --archive="$DIRNAME/backup/vline-trips.bson.gz"
