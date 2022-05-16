dba83b64804985eb83e98e0faa8d574c489bee4d
Eyeball Edward <edwardyeung39@gmail.com>
2022-05-16 22:21:36 +0800

    cleanup bike data too

---
+++ additional-data/metro-data/geospatial/generate-station-bike-storage.js
@@ -0,0 +1,57 @@
+const fs = require('fs')
+const path = require('path')
+const utils = require('../../../utils')
+const async = require('async')
+const config = require('../../../config')
+const DatabaseConnection = require('../../../database/DatabaseConnection')
+
+const database = new DatabaseConnection(config.databaseURL, config.databaseName)
+
+let bikesJSON = JSON.parse(fs.readFileSync(process.argv[2]).toString())
+let output = {}
+
+database.connect(async () => {
+  let stops = database.getCollection('stops')
+
+  await async.forEachSeries(bikesJSON.features, async bikeStorage => {
+    let locationName = bikeStorage.properties.STATION
+    let capacity = bikeStorage.properties.CAPACITY
+    let type = bikeStorage.properties.TYPE
+    let location = bikeStorage.properties.LOCATION
+    let geometry = bikeStorage.geometry
+
+    if (locationName.includes('Train Facility')) return console.log('Skipping bike storage for', locationName)
+
+    let firstStop = geometry.coordinates
+
+    let closestStation = await stops.findDocument({
+      location: {
+        $nearSphere: {
+          $geometry: {
+            type: 'Point',
+            coordinates: firstStop
+          },
+          $maxDistance: 200
+        }
+      },
+      'bays.mode': {
+        $in: ['metro train', 'regional train']
+      }
+    })
+
+    if (!closestStation) return console.log(bikeStorage);
+    let station = closestStation.stopName.slice(0, -16)
+
+    if (!output[station]) output[station] = []
+    output[station].push({
+      capacity,
+      type,
+      location,
+      geometry
+    })
+  })
+
+  fs.writeFileSync(path.join(__dirname, 'station-bikes.json'), JSON.stringify(output))
+
+  process.exit(0)
+})
--- application/routes/StopPreview.js
+++ application/routes/StopPreview.js
@@ -4,7 +4,7 @@ const router = new express.Router()
 const bayData = require('../../additional-data/bus-bays')
 const trainReplacementBays = require('../../additional-data/train-replacement-bays')
 const platformGeometry = require('../../additional-data/station-platform-geometry')
-const stationBikes = require('../../additional-data/station-bikes')
+const stationBikes = require('../../additional-data/metro-data/geospatial/station-bikes')
 const stationCarparks = require('../../additional-data/metro-data/geospatial/station-carparks')
 const turf = require('@turf/turf')
 
