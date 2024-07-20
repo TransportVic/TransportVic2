cd3efb1af6467cc6f7e8bf95961c2ca6b95aa038
Eyeball Edward <edwardyeung39@gmail.com>
2024-07-20 20:56:42 +1000

    update consist updater

--- update-consists.js
+++ update-consists.js
@@ -17,9 +17,9 @@ utils.request(urls.ptDatabaseMetro).then(data => {
     .filter(train => trainType[train.trainType])
     .map(train => ({
       type: train.trainType,
-      consist: [train.mCarA, train.tCar, train.mCarB]
+      consist: [train.m1, train.t1, train.m2]
     }))
-    .filter(train => !train.consist.includes('-'))
+    .filter(train => !(train.consist.includes('-') || train.consist.includes('')))
 
   let types = validTrains.map(train => ({
     leadingCar: train.consist[0],
@@ -34,12 +34,12 @@ utils.request(urls.ptDatabaseMetro).then(data => {
   utils.request(urls.ptDatabaseHCMT).then(hcmtData => {
     let parsedHCMT = JSON.parse(hcmtData)
     consists = consists.concat(parsedHCMT.map(train => {
-      let rawTrain = [train.TC1, train.DMP1, train.MP1, train.DT1, train.DMP2, train.DT2, train.MP3, train.DMP3, train.TC2]
+      let rawTrain = [train.tc1, train.dmp1, train.mp1, train.dt1, train.dmp2, train.dt2, train.mp3, train.dmp3, train.tc2]
       return rawTrain.filter(x => x !== '-' && x)
     }))
 
     types = types.concat(parsedHCMT.map(train => ({
-      leadingCar: train.TC1,
+      leadingCar: train.tc1,
       type: 'HCMT'
     })))
 
