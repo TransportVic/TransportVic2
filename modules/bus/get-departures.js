ad97cac5329f560dc0340a362f337710846e2729
Eyeball Edward <edwardyeung39@gmail.com>
2024-07-20 21:08:15 +1000

    add melb uni into the fetch-all list

--- modules/bus/get-departures.js
+++ modules/bus/get-departures.js
@@ -81,7 +81,8 @@ async function getDeparturesFromPTV(stop, db, time, discardUnmatched) {
     'Ringwood Railway Station',
     'Chelsea Railway Station',
     'Tarneit Railway Station',
-    'Hallam Railway Station'
+    'Hallam Railway Station',
+    'Melbourne University Grattan Street Stop/Grattan Street'
   ]
   if (affectedStops.includes(stop.stopName)) {
     uniqueStops = stop.bays.filter(b => b.mode === 'bus').map(b => b.stopGTFSID).filter(b => {
