57188d503f6195b76e0cc362ffd862fe9d8267d6
Eyeball Edward <edwardyeung39@gmail.com>
2025-03-29 18:44:46 +1100

    added new metro trains folder

---
+++ modules/metro-trains/get-departures.js
@@ -0,0 +1,12 @@
+async function fetchLiveTrips(station, db, departureTime) {
+  
+}
+
+async function getDepartures(station, db, options={}) {
+  let { lookBackwards, departureTime } = options
+}
+
+module.exports = {
+  fetchLiveTrips,
+  getDepartures
+}
