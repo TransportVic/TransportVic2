56392c75cd83e72cd6ab311e8c607f01f52f47af
Eyeball Edward <edwardyeung39@gmail.com>
2025-03-29 20:28:04 +1100

    added DST test

--- modules/metro-trains/test/sch-to-live.test.mjs
+++ modules/metro-trains/test/sch-to-live.test.mjs
@@ -16,4 +16,13 @@ describe('The convertToLive function', () => {
     expect(trip.stopTimings[6].stopName).to.equal('Camberwell Railway Station')
     expect(trip.stopTimings[6].scheduledDepartureTime).to.equal('2025-03-28T22:19:00.000Z')
   })
+
+  it('Should account for a DST repeated 2am and add an extra hour', async () => {
+    let trip = convertToLive(regSchTrip, new Date('2025-04-05T21:00:00.000Z'))
+    expect(trip.stopTimings[0].stopName).to.equal('Alamein Railway Station')
+    expect(trip.stopTimings[0].scheduledDepartureTime).to.equal('2025-04-05T23:08:00.000Z')
+
+    expect(trip.stopTimings[6].stopName).to.equal('Camberwell Railway Station')
+    expect(trip.stopTimings[6].scheduledDepartureTime).to.equal('2025-04-05T23:19:00.000Z')
+  })
 })
