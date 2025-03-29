69c12f50d158d521f28b4b88c692220c24327d11
Eyeball Edward <edwardyeung39@gmail.com>
2025-03-29 22:24:18 +1100

    search previous day for departures as well

--- modules/metro-trains/get-departures.js
+++ modules/metro-trains/get-departures.js
@@ -1,3 +1,4 @@
+const utils = require('../../utils')
 const { getNonDSTMinutesPastMidnight } = require('../dst/dst')
 const { convertToLive } = require('./sch-to-live')
 
@@ -49,28 +50,39 @@ async function fetchScheduledTrips(station, db, departureTime, timeframe=120, ba
 
   // Get the non DST aware minutes past midnight - the times would be saved normally
   let rawDepartureTimeMinutes = getNonDSTMinutesPastMidnight(departureTime)
+  let departureDay = utils.parseTime(departureTime).startOf('day')
 
-  let departureTimeMinutes = backwards ? {
-    $gte: rawDepartureTimeMinutes - timeframe,
-    $lte: rawDepartureTimeMinutes
-  } : {
-    $gte: rawDepartureTimeMinutes - 1,
-    $lte: rawDepartureTimeMinutes + timeframe
+  let trips = []
+
+  for (let i = 1; i >= 0; i--) { // Run in reverse to get a naturally sorted order
+    let minutesOffset = i * 1440 // Note - fix as 1440 first and have a think if it needs to be dynamic
+    let operationDay = departureDay.clone().add(-i, 'days')
+
+    let departureTimeMinutes = backwards ? {
+      $gte: rawDepartureTimeMinutes - timeframe + minutesOffset,
+      $lte: rawDepartureTimeMinutes + minutesOffset
+    } : {
+      $gte: rawDepartureTimeMinutes - 1 + minutesOffset,
+      $lte: rawDepartureTimeMinutes + timeframe + minutesOffset
+    }
+
+    let dayTrips = await gtfsTimetables.findDocuments({
+      mode: 'metro train',
+      operationDays: utils.getYYYYMMDD(operationDay),
+      stopTimings: {
+        $elemMatch: {
+          stopGTFSID: {
+            $in: stopGTFSIDs
+          },
+          departureTimeMinutes
+        }
+      },
+    }).toArray()
+
+    trips.push(...dayTrips.map(trip => convertToLive(trip, operationDay)))
   }
 
-  let trips = await gtfsTimetables.findDocuments({
-    mode: 'metro train',
-    stopTimings: {
-      $elemMatch: {
-        stopGTFSID: {
-          $in: stopGTFSIDs
-        },
-        departureTimeMinutes
-      }
-    },
-  }).toArray()
-
-  return trips.map(trip => convertToLive(trip, departureTime))
+  return trips
 }
 
 async function getDepartures(station, db, options={}) {
