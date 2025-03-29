acd9708a879db10ef96401dc2b17c94c45c0a0ca
Eyeball Edward <edwardyeung39@gmail.com>
2025-03-29 20:32:51 +1100

    modified DST class to return an offset as well

--- modules/dst/dst.js
+++ modules/dst/dst.js
@@ -1,12 +1,21 @@
 const moment = require('moment-timezone')
 moment.tz.setDefault('Australia/Melbourne')
 
-module.exports = function isDSTChange(date) {
+function isDSTChange(date) {
+  return getTimeOffset(date) !== 0
+}
+
+function getTimeOffset(date) {
   let givenDay = moment(date)
   let previousDay = givenDay.clone().add(-1, 'day')
 
   let previousUTCOffset = previousDay.utcOffset()
   let givenUTCOffset = givenDay.utcOffset()
 
-  return previousUTCOffset !== givenUTCOffset
+  return previousUTCOffset - givenUTCOffset
+}
+
+module.exports = {
+  isDSTChange,
+  getTimeOffset
 }
--- modules/dst/test/dst.test.mjs
+++ modules/dst/test/dst.test.mjs
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import isDSTChange from '../dst.js'
+import { getTimeOffset, isDSTChange } from '../dst.js'
 
 describe('The DST function', () => {
   it('Should identify if a given day is the first day of a time change', () => {
@@ -10,5 +10,11 @@ describe('The DST function', () => {
     expect(isDSTChange(new Date('2024-10-05T08:59:53.716Z'))).to.be.false
     expect(isDSTChange(new Date('2024-10-06T08:59:53.716Z'))).to.be.true // 2am is skipped this day
     expect(isDSTChange(new Date('2024-10-07T08:59:53.716Z'))).to.be.false
+  })
+
+  it('Should return the HH:MM and minutes offset on a given day', () => {
+    expect(getTimeOffset(new Date('2025-03-29T08:59:53.716Z'))).to.equal(0)
+    expect(getTimeOffset(new Date('2025-04-06T08:59:53.716Z'))).to.equal(60) // Extra 60 minutes to account for the repeated 2am
+    expect(getTimeOffset(new Date('2024-10-06T08:59:53.716Z'))).to.equal(-60) // 60 less minutes to account for the skipped 2am
   })
 })
