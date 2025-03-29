e1583096323a6ea8b0f0bc3683e1d3864cdeb018
Eyeball Edward <edwardyeung39@gmail.com>
2025-03-29 21:55:22 +1100

    added test for next PT day

--- modules/metro-trains/test/departures.test.mjs
+++ modules/metro-trains/test/departures.test.mjs
@@ -42,4 +42,12 @@ describe('The fetchScheduledTrips function', () => {
     expect(trips[1].stopTimings[0].scheduledDepartureTime).to.equal('2025-03-29T13:16:00.000Z') // 00:16 NEXT DAY
     expect(trips[2].stopTimings[0].scheduledDepartureTime).to.equal('2025-03-29T14:14:00.000Z') // 01:14 NEXT DAY
   })
+
+  it('Should match trips across to the next day', async () => {
+    let trips = await fetchScheduledTrips(alamein, midnightDBNoDST, new Date('2025-03-29T15:10:00.000Z')) // 20250330 02:10 NEXT DAY from 20250329
+    expect(trips.length).to.equal(2) // 02:16 and 03:18
+
+    expect(trips[0].stopTimings[0].scheduledDepartureTime).to.equal('2025-03-29T15:16:00.000Z') // 02:16 NEXT DAY
+    expect(trips[1].stopTimings[0].scheduledDepartureTime).to.equal('2025-03-29T16:18:00.000Z') // 03:16 NEXT PT DAY
+  })
 })
