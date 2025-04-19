abf8d82a05c718b7ec167307428dca4d672e3b35
Eyeball Edward <edwardyeung39@gmail.com>
2025-04-19 21:46:56 +1000

    added direction/runID getter/setter

--- modules/schema/live-timetable.js
+++ modules/schema/live-timetable.js
@@ -9,6 +9,9 @@ module.exports = class LiveTimetable {
   #tripID
   #block
 
+  #runID
+  #direction
+
   constructor(mode, operationDays, routeName, routeGTFSID, tripID, block) {
     this.#mode = mode
     this.#operationDay = utils.parseDate(operationDays)
@@ -17,6 +20,20 @@ module.exports = class LiveTimetable {
     this.#tripID = tripID
     this.#block = block
   }
+
+  get mode() { return this.#mode }
+  get routeGTFSID() { return this.#routeGTFSID }
+  get routeName() { return this.#routeName }
+  get operationDay() { return utils.getYYYYMMDD(this.#operationDay) }
+  get operationDayMoment() { return this.#operationDay.clone() }
+  get tripID() { return this.#tripID }
+  get block() { return this.#block }
+  get direction() { return this.#direction }
+  get runID() { return this.#runID }
+
+  set direction(direction) { this.#direction = direction }
+  set runID(runID) { this.#runID = runID }
+
 
   static fromDatabase(timetable) {
     let timetableInstance = new LiveTimetable(
@@ -27,6 +44,9 @@ module.exports = class LiveTimetable {
       timetable.tripID,
       timetable.block
     )
+
+    if (timetable.direction) timetableInstance.direction = timetable.direction
+    if (timetable.runID) timetableInstance.runID = timetable.runID
 
     return timetableInstance
   }
