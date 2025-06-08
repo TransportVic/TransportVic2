292734c3275bfe8568e6dfcbea29d07d065844ee
Eyeball Edward <edwardyeung39@gmail.com>
2025-06-08 23:52:05 +1000

    implemented gtfsr tracker

--- modules/new-tracker/metro-gtfsr-trips.mjs
+++ modules/new-tracker/metro-gtfsr-trips.mjs
@@ -6,7 +6,7 @@ import LiveTimetable from '../schema/live-timetable.js'
 import { MongoDatabaseConnection } from '@transportme/database'
 import config from '../../config.json' with { type: 'json' }
 import { MetroGTFSRTrip } from './GTFSRTrip.mjs'
-import { getStop } from './trip-updater.mjs'
+import { getStop, updateTrip } from './trip-updater.mjs'
 
 export async function getUpcomingTrips(db, gtfsrAPI) {
   let tripData = await gtfsrAPI('metrotrain-tripupdates')
@@ -43,71 +43,15 @@ export async function getUpcomingTrips(db, gtfsrAPI) {
   return output
 }
 
-async function getRoute(db, routeName) {
-  let routes = db.getCollection('routes')
-  let route = await routes.findDocument({
-    routeName
-  })
-
-  return route
-}
-
-async function getTrip(liveTimetables, runID, date) {
-  return await liveTimetables.findDocument({
-    mode: 'metro train',
-    runID,
-    operationDays: date
-  })
-}
-
-async function createTrip(trip, db) {
-  let routeData = await getRoute(db, trip.routeName)
-
-  let timetable = new LiveTimetable(
-    'metro train',
-    trip.operationalDate,
-    trip.routeName,
-    routeData.routeNumber,
-    routeData.routeGTFSID,
-    null,
-    null
-  )
-
-  timetable.runID = trip.tdn
-  timetable.direction = trip.tdn[3] % 2 === 0 ? 'Up' : 'Down'
-  timetable.forming = trip.forming
-  timetable.formedBy = trip.formedBy
-
-  for (let stop of trip.stops) {
-    let stopData = await getStop(db, stop.stationName + ' Railway Station')
-    let platformBay = stopData.bays.find(bay => bay.mode === 'metro train' && bay.platform === stop.platform)
-
-    timetable.updateStopByName(stopData.stopName, {
-      stopGTFSID: platformBay.stopGTFSID,
-      stopNumber: null,
-      suburb: platformBay.suburb,
-      scheduledDepartureTime: stop.scheduledDeparture.toUTC().toISO(),
-      platform: stop.platform
-    })
-  }
-
-  return timetable
-}
-
 export async function fetchTrips(db) {
   let relevantTrips = await getUpcomingTrips(db, makePBRequest)
-  let liveTimetables = db.getCollection('live timetables')
 
-  let tripObjects = {}
-  utils.inspect(relevantTrips)
-  // for (let trip of relevantTrips) {
-  //   let tripData = await getTrip(liveTimetables, trip.tdn, trip.operationalDate)
-
-  //   if (tripData) tripObjects[trip.tdn] = LiveTimetable.fromDatabase(tripData)
-  //   else tripObjects[trip.tdn] = await createTrip(trip, db)
-  // }
-
-  // await liveTimetables.bulkWrite(bulkUpdate)
+  for (let tripData of relevantTrips) {
+    // TODO: Implement separate arrival/departure times
+    let lastStop = tripData.stops[tripData.stops.length - 1]
+    lastStop.estimatedDepartureTime = lastStop.estimatedArrivalTime
+    await updateTrip(db, tripData)
+  }
 }
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
