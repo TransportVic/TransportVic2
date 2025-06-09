34963f83c6720e800c90bcc274e90bb2eb98df4d
Eyeball Edward <edwardyeung39@gmail.com>
2025-06-10 08:45:09 +1000

    added test for fetching missing trips

--- modules/metro-trains/test/ptv-api-departures.test.mjs
+++ modules/metro-trains/test/ptv-api-departures.test.mjs
@@ -1,7 +1,8 @@
 import { expect } from 'chai'
 import ptvRWD from '../../new-tracker/test/sample-data/rwd-ptv-departures.json' with { type: 'json' }
 import rwdStop from '../../new-tracker/test/sample-data/rwd-stop-db.json' with { type: 'json' }
-import LILTD3826 from '../../new-tracker/test/sample-data/lil-3826.json' with { type: 'json' }
+import TD3826DBTrip from '../../new-tracker/test/sample-data/lil-3826.json' with { type: 'json' }
+import ptvAPITD3213 from '../../new-tracker/test/sample-data/lil-3826.json' with { type: 'json' }
 import { PTVAPI, StubAPI } from '@transportme/ptv-api'
 import getTripUpdateData from '../get-ptv-departures.js'
 import { LokiDatabaseConnection } from '@transportme/database'
@@ -16,7 +17,7 @@ describe('The getTripUpdateData function', () => {
     let liveTimetables = await database.createCollection('live timetables')
 
     await stops.createDocument(clone(rwdStop))
-    await liveTimetables.createDocument(clone(LILTD3826))
+    await liveTimetables.createDocument(clone(TD3826DBTrip))
     await routes.createDocuments([{
       "mode" : "metro train",
       "routeName" : "Lilydale",
@@ -60,6 +61,76 @@ describe('The getTripUpdateData function', () => {
       stopName: 'Ringwood Railway Station',
       platform: '2',
       estimatedDepartureTime: new Date('2025-06-09T22:07:43.000Z'),
+      cancelled: false
+    })
+  })
+
+  it('Should fetch stopping pattern data for unmatched trips in the database', async () => {
+    let database = new LokiDatabaseConnection('test-db')
+    let stops = await database.createCollection('stops')
+    let routes = await database.createCollection('routes')
+    let liveTimetables = await database.createCollection('live timetables')
+
+    await stops.createDocument(clone(rwdStop))
+    await liveTimetables.createDocument(clone(TD3826DBTrip))
+    await routes.createDocuments([{
+      "mode" : "metro train",
+      "routeName" : "Lilydale",
+      "cleanName" : "lilydale",
+      "routeNumber" : null,
+      "routeGTFSID" : "2-LIL",
+      "operators" : [
+        "Metro"
+      ],
+      "codedName" : "lilydale"
+    }, {
+      "mode" : "metro train",
+      "routeName" : "Belgrave",
+      "cleanName" : "belgrave",
+      "routeNumber" : null,
+      "routeGTFSID" : "2-BEG",
+      "operators" : [
+        "Metro"
+      ],
+      "codedName" : "belgrave"
+    }])
+
+    let stubAPI = new StubAPI()
+    let response = clone(ptvRWD)
+    response.departures = [ response.departures[0], response.departures[1] ]
+    stubAPI.setResponses([ response, clone(ptvAPITD3213) ])
+    let ptvAPI = new PTVAPI(stubAPI)
+
+    let tripData = await getTripUpdateData(database, rwdStop, ptvAPI)
+
+    expect(tripData[1].operationDays).to.equal('20250610')
+    expect(tripData[1].runID).to.equal('3213')
+    expect(tripData[1].routeGTFSID).to.equal('2-LIL')
+    expect(tripData[1].cancelled).to.be.false
+
+    expect(tripData[1].formedBy).to.equal('2806')
+    expect(tripData[1].forming).to.equal('3832')
+
+    expect(tripData[1].stops[0]).to.deep.equal({
+      stopName: 'Flinders Street Railway Station',
+      platform: '3',
+      scheduledDepartureTime: new Date('2025-06-10T07:29:00.000+10:00'),
+      estimatedDepartureTime: new Date('2025-06-10T07:28:40.000+10:00'),
+      cancelled: false
+    })
+
+    expect(tripData[1].stops[tripData[1].stops.length - 2]).to.deep.equal({
+      stopName: 'Mooroolbark Railway Station',
+      platform: '2',
+      scheduledDepartureTime: new Date('2025-06-10T08:22:00.000+10:00'),
+      estimatedDepartureTime: new Date('2025-06-10T08:24:00.000+10:00'),
+      cancelled: false
+    })
+
+    expect(tripData[1].stops[tripData[1].stops.length - 1]).to.deep.equal({
+      stopName: 'Lilydale Railway Station',
+      platform: '1',
+      scheduledDepartureTime: new Date('2025-06-10T08:28:00.000+10:00'),
       cancelled: false
     })
   })
