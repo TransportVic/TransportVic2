151e401d4ff3b13f3ef132741db50d48af1a7d08
Eyeball Edward <edwardyeung39@gmail.com>
2025-06-11 14:15:47 +1000

    ensure changes in text generate a new alert

--- modules/new-tracker/test/metro-notify.test.mjs
+++ modules/new-tracker/test/metro-notify.test.mjs
@@ -63,4 +63,34 @@ describe('The MetroNotify tracker', () => {
     expect(alert.rawAlertID).to.equal('647069')
     expect(alert.active).to.be.false
   })
+
+  it('Should create a new alert if the text is updated', async () => {
+    let database = new LokiDatabaseConnection('test-db')
+    let metroNotify = await database.createCollection('metro notify')
+
+    let secondResponse = clone(notifyAlerts)
+    secondResponse[87].alerts[0].alert_text = secondResponse[87].alerts[0].alert_text.replace('10', '20')
+
+    let api = createAPI([ notifyAlerts, secondResponse ])
+
+    expect(await metroNotify.countDocuments({})).to.equal(0)
+    await fetchNotifyAlerts(api, database)
+    expect(await metroNotify.countDocuments({})).to.equal(1)
+    let alert = await metroNotify.findDocument({ active: true })
+
+    expect(alert.rawAlertID).to.equal('647069')
+    expect(alert.active).to.be.true
+
+    await fetchNotifyAlerts(api, database)
+    expect(await metroNotify.countDocuments({})).to.equal(2)
+    let oldAlert = await metroNotify.findDocument({ active: false })
+    let newAlert = await metroNotify.findDocument({ active: true })
+    expect(oldAlert).to.equal(alert)
+    expect(oldAlert.text).to.equal('<p>The 8:46am Flinders Street to Epping service is currently running up to 10 minutes late.</p>')
+
+    expect(newAlert.rawAlertID).to.equal('647069')
+    expect(newAlert.alertID).to.not.equal(oldAlert.alertID)
+    expect(newAlert.active).to.be.true
+    expect(newAlert.text).to.equal('<p>The 8:46am Flinders Street to Epping service is currently running up to 20 minutes late.</p>')
+  })
 })
