acb1c82f5449403aeb055bfb3882430e9224bcec
Eyeball Edward <edwardyeung39@gmail.com>
2025-06-12 14:45:45 +1000

    consolidate metro tracker run script

---
+++ modules/new-tracker/metro.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DIRNAME=$(dirname "$0")
+
+node $DIRNAME/metro/metro-gtfsr-trips.mjs
+node $DIRNAME/metro/metro-trips-departures.mjs
+node $DIRNAME/metro/metro-ptv-departures.mjs
+node $DIRNAME/metro/metro-ptv-trips.mjs
+node $DIRNAME/metro/metro-notify.mjs
+node $DIRNAME/metro/metro-notify-trips.mjs
+node $DIRNAME/metro/metro-cbd-ptv-departures.mjs
