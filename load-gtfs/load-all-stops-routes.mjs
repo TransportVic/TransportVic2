61edfca1ecb226b818af0349f5ddc8ccab126a80
Eyeball Edward <edwardyeung39@gmail.com>
2025-06-14 03:36:06 +1000

    update packages

--- load-gtfs/load-all-stops-routes.mjs
+++ load-gtfs/load-all-stops-routes.mjs
@@ -95,6 +95,7 @@ for (let i of selectedModes) {
 
     console.log('Loaded routes for', GTFS_MODES[i])
   } catch (e) {
+    console.log(e)
     console.log('Failed to load stops and routes for', GTFS_MODES[i])
   }
 }
