a8456cb6701d716bb7a19070592821e31a960cfa
Eyeball Edward <edwardyeung39@gmail.com>
2025-06-13 11:56:31 +1000

    check for MTM-MTM leading carriages

--- modules/metro-trains/fleet-parser.js
+++ modules/metro-trains/fleet-parser.js
@@ -6,5 +6,20 @@ module.exports.getLeadingVehicles =  function getLeadingVehicles(consist) {
   let carriages = consist.split('-')
   if (carriages[0].match(/9\d{3}M/)) { // HCMT
     return [ carriages[0].slice(0, 4) ]
+  } else if (consist.includes('M')) {
+    let mCars = carriages.filter(carriage => carriage.endsWith('M')).map(carriage => ({
+      number: parseInt(carriage.slice(0, -1)),
+      carriage
+    })).sort((a, b) => a.number - b.number)
+    let leadingVehicles = []
+    for (let i = 0; i < mCars.length - 1; i++) {
+      let { number, carriage } = mCars[i]
+      let nextNumber = mCars[i + 1].number
+      if ((number % 2 === 1) && (nextNumber === number + 1)) {
+        leadingVehicles.push(carriage)
+        i++
+      }
+    }
+    return leadingVehicles
   }
 }
