eff16cdf234e0f7def8973612275274a8947a2da
Eyeball Edward <edwardyeung39@gmail.com>
2025-06-21 19:31:48 +1000

    new maintenance scripts

--- start-maintenance-light.sh
+++ start-maintenance-light.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 DIRNAME=$(dirname "$0")
 
-sudo systemctl stop transportvic
+
 sudo systemctl restart mongod
 node "$DIRNAME/timetable-updating-server/index.js" 2>&1 | tee "$DIRNAME/updater-log.txt"
 sudo systemctl restart mongod
-sudo systemctl start transportvic
+sudo systemctl restart transportvic
