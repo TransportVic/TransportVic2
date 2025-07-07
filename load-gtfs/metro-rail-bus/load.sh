eefddab6bf66e1dae2269f4bec1a9c5ad53bbd73
Eyeball Edward <edwardyeung39@gmail.com>
2025-07-08 01:06:33 +1000

    add load script

---
+++ load-gtfs/metro-rail-bus/load.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DIRNAME=$(dirname "$0")
+. $HOME/.bash_profile
+
+node $DIRNAME/download-data.mjs
+node $DIRNAME/load-all-stops-routes.mjs
+node $DIRNAME/load-all-trips.mjs
+node $DIRNAME/load-op-timetable.mjs
