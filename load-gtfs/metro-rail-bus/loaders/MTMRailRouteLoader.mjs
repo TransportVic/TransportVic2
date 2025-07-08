f86dd5467af73d709876e9629fa8bd7f5830410b
Eyeball Edward <edwardyeung39@gmail.com>
2025-07-08 15:55:42 +1000

    move custom class from gtfs package to here

--- load-gtfs/metro-rail-bus/loaders/MTMRailRouteLoader.mjs
+++ load-gtfs/metro-rail-bus/loaders/MTMRailRouteLoader.mjs
@@ -1,5 +1,38 @@
-import { RouteLoader } from '@transportme/load-ptv-gtfs'
+import { GTFSReaders, GTFSTypes, RouteLoader } from '@transportme/load-ptv-gtfs'
 import { GTFS_CONSTANTS } from '@transportme/transportvic-utils'
+
+export class MTMRailRoute extends GTFSTypes.GTFSRoute {
+
+  constructor(data) {
+    super(data)
+    this.routeName = 'Rail Replacement Bus'
+    this.routeNumber = null
+  }
+
+  static canProcess(routeGTFSID, mode) {
+    return !routeGTFSID.match(/^\d+-\w{1,3}/) && mode === 'metro train'
+  }
+
+  parseRouteID() {
+    return '2-RRB'
+  }  
+
+}
+
+export class MTMRailRouteReader extends GTFSReaders.GTFSRouteReader {
+
+  processEntity(data) {
+    let routeData = {
+      routeGTFSID: data.route_id,
+      agencyID: data.agency_id,
+      routeNumber: data.route_short_name,
+      routeName: data.route_long_name
+    }
+
+    return MTMRailRoute(routeData, this.getMode())
+  }
+
+}
 
 export default class MTMRailRouteLoader extends RouteLoader {
 
--- load-gtfs/metro-rail-bus/loaders/MTMRailTripLoader.mjs
+++ load-gtfs/metro-rail-bus/loaders/MTMRailTripLoader.mjs
@@ -1,6 +1,46 @@
 import { GTFSReaders, GTFSTypes, TripLoader } from '@transportme/load-ptv-gtfs'
 import { GTFS_CONSTANTS } from '@transportme/transportvic-utils'
 
+export class MTMRailTrip extends GTFSTypes.GTFSTrip {
+
+  constructor(data) {
+    if (data.headsign && data.headsign.length === 6 && data.headsign.match(/[A-Z]{3}\d{3}/)) {
+      data.block = data.headsign
+    }
+
+    super(data)
+  }
+
+  static canProcess(data, mode) {
+    return mode === TRANSIT_MODES.metroTrain && data.routeGTFSID === '2-RRB'
+  }
+
+  getTripData() {
+    return {
+      ...super.getTripData(),
+      isRailReplacementBus: true
+    }
+  }
+}
+
+export class MTMTripReader extends GTFSReaders.GTFSTripReader {
+
+  processEntity(data) {
+    let tripData = {
+      routeGTFSID: this.getRouteMapping(data.route_id),
+      calendar: this.getCalendar(data.service_id),
+      id: data.trip_id,
+      shapeID: data.shape_id,
+      headsign: data.trip_headsign,
+      direction: data.direction_id,
+      block: data.block_id
+    }
+
+    return new MTMRailTrip(tripData, this.getMode())
+  }
+
+}
+
 export class MTMRailStopTimesReader extends GTFSReaders.GTFSStopTimesReader {
 
   processEntity(data) {
@@ -39,6 +79,10 @@ export default class MTMRailTripLoader extends TripLoader {
 
   createStopTimesReader(stopTimesFile, mode) {
     return new MTMRailStopTimesReader(stopTimesFile, mode)
+  }
+
+  createTripReader(tripsFile, calendars, routeMappings, mode) {
+    return new MTMTripReader(tripsFile, calendars, routeMappings, mode)
   }
 
   async loadTrips(routeIDMap) {
