00a1cae965d51d3490796c66044a9ce771699ac8
Eyeball Edward <edwardyeung39@gmail.com>
2025-07-08 20:38:12 +1000

    add trip data required for update to be processed

--- modules/new-tracker/metro-rail-bus/test/rt-data.test.mjs
+++ modules/new-tracker/metro-rail-bus/test/rt-data.test.mjs
@@ -1,9 +1,11 @@
 import { LokiDatabaseConnection } from '@transportme/database'
 import dbStops from '../../metro/test/sample-data/pkm-stops-db.json' with { type: 'json' }
+import dbTrips from './sample-data/trip.json' with { type: 'json' }
 import updates from './sample-data/rt-updates.json' with { type: 'json' }
 import { PTVAPI, StubAPI } from '@transportme/ptv-api'
 import { getRailBusUpdates } from '../load-rt-updates.mjs'
 import { expect } from 'chai'
+import getTripID from '../get-trip-id.mjs'
 
 let clone = o => JSON.parse(JSON.stringify(o))
 
@@ -22,14 +24,16 @@ describe('The MTM Rail Bus Tracker', () => {
   it('Should create a trip updater object', async () => {
     let database = new LokiDatabaseConnection('test-db')
     let stops = await database.createCollection('stops')
+    let timetables = await database.createCollection('live timetables')
+
     await stops.createDocuments(clone(dbStops))
+    await timetables.createDocument(clone(dbTrips))
 
     let tripData = await getRailBusUpdates(database, createAPI())
-
-    expect(tripData[0].tripID).to.equal('Mon - Tue_qo16765')
+    expect(tripData[0].runID).to.equal(getTripID('Mon - Tue_qo16765'))
     expect(tripData[0].routeGTFSID).to.equal('2-RRB')
     expect(tripData[0].stops[0].stopName).to.equal('Carnegie Railway Station')
-    expect(tripData[0].stops[0].estimatedArrivalTime.toISOString()).to.equal('2025-07-08T06:04:27.000Z')
-    expect(tripData[0].stops[0].estimatedDepartureTime.toISOString()).to.equal('2025-07-08T06:04:27.000Z')
+    expect(tripData[0].stops[0].estimatedArrivalTime.toISOString()).to.equal('2025-07-08T06:07:24.000Z')
+    expect(tripData[0].stops[0].estimatedDepartureTime.toISOString()).to.equal('2025-07-08T06:07:24.000Z')
   })
 })
