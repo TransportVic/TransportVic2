1e681ee1fb02b95480b3c692703360f1ebe4183d
Eyeball Edward <edwardyeung39@gmail.com>
2025-07-13 12:48:21 +1000

    stub time in rail bus test

--- modules/new-tracker/metro-rail-bus/test/rt-data.test.mjs
+++ modules/new-tracker/metro-rail-bus/test/rt-data.test.mjs
@@ -6,6 +6,7 @@ import { PTVAPI, StubAPI } from '@transportme/ptv-api'
 import { getRailBusUpdates } from '../load-rt-updates.mjs'
 import { expect } from 'chai'
 import getTripID from '../get-trip-id.mjs'
+import utils from '../../../../utils.js'
 
 let clone = o => JSON.parse(JSON.stringify(o))
 
@@ -21,6 +22,12 @@ function createAPI() {
 }
 
 describe('The MTM Rail Bus Tracker', () => {
+  let originalNow
+  before(() => {
+    originalNow = utils.now
+    utils.now = () => utils.parseTime(1751954847 * 1000)
+  })
+
   it('Should create a trip updater object', async () => {
     let database = new LokiDatabaseConnection('test-db')
     let stops = await database.createCollection('stops')
@@ -35,5 +42,9 @@ describe('The MTM Rail Bus Tracker', () => {
     expect(tripData[0].stops[0].stopName).to.equal('Carnegie Railway Station')
     expect(tripData[0].stops[0].estimatedArrivalTime.toISOString()).to.equal('2025-07-08T06:07:24.000Z')
     expect(tripData[0].stops[0].estimatedDepartureTime.toISOString()).to.equal('2025-07-08T06:07:24.000Z')
+  })
+
+  after(() => {
+    utils.now = originalNow
   })
 })
