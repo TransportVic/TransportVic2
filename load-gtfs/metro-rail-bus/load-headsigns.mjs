d9a2b1396a4aace893a9a6669eff76f859ed446f
Eyeball Edward <edwardyeung39@gmail.com>
2025-07-16 19:16:59 +1000

    account for index file being blank

--- load-gtfs/metro-rail-bus/load-headsigns.mjs
+++ load-gtfs/metro-rail-bus/load-headsigns.mjs
@@ -4,6 +4,13 @@ import { GTFS_CONSTANTS } from '@transportme/transportvic-utils'
 import utils from '../../utils.js'
 import urls from '../../urls.json' with { type: 'json' }
 import config from '../../config.json' with { type: 'json' }
+import stationCodes from '../../additional-data/station-codes.json' with { type: 'json' }
+
+let stationCodeLookup = {}
+
+Object.keys(stationCodes).forEach(stationCode => {
+  stationCodeLookup[stationCodes[stationCode]] = stationCode
+})
 
 let database = new MongoDatabaseConnection(config.databaseURL, config.databaseName)
 await database.connect()
@@ -14,6 +21,21 @@ let stoppingPatterns = JSON.parse(await utils.request(urls.metroRRBIndex)).stopp
 let stopFrequency = {}
 let patterns = {}
 let patternsAtStop = {}
+
+if (!stoppingPatterns.length) {
+  let allTrips = await timetables.findDocuments({ mode: GTFS_CONSTANTS.TRANSIT_MODES.metroTrain, routeGTFSID: '2-RRB' }).toArray()
+  let patterns = {}
+  for (let trip of allTrips) {
+    let pattern = trip.stopTimings.map(stop => stationCodeLookup[stop.stopName.slice(0, -16)])
+    let patternID = pattern.join('-')
+    if (patterns[patternID]) continue
+    patterns[patternID] = {
+      id: patternID,
+      stops: pattern
+    }
+  }
+  stoppingPatterns = Object.values(patterns)
+}
 
 for (let pattern of stoppingPatterns) {
   patterns[pattern.id] = pattern
