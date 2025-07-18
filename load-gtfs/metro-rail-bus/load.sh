af443a036a3db2710d2e17749b335333512da17c
Eyeball Edward <edwardyeung39@gmail.com>
2025-07-19 00:32:59 +1000

    add error check

--- load-gtfs/metro-rail-bus/load.sh
+++ load-gtfs/metro-rail-bus/load.sh
@@ -2,8 +2,20 @@
 DIRNAME=$(dirname "$0")
 . $HOME/.bash_profile
 
-node $DIRNAME/download-data.mjs
-node $DIRNAME/load-all-stops-routes.mjs
-node $DIRNAME/load-all-trips.mjs
-node $DIRNAME/load-headsigns.mjs
-node $DIRNAME/load-op-timetable.mjs
+cd $DIRNAME
+
+node download-data.mjs
+EC1=$?
+node load-all-stops-routes.mjs
+EC2=$?
+node load-all-trips.mjs
+EC3=$?
+node load-headsigns.mjs
+EC4=$?
+node load-op-timetable.mjs
+EC5=$?
+
+if [[ $EC1 -ne 0 ]] || [[ $EC2 -ne 0 ]] || [[ $EC3 -ne 0 ]] || [[ $EC4 -ne 0 ]] || [[ $EC5 -ne 0 ]]; then
+  echo "At least 1 script failed"
+  node -e "require('../../modules/discord-integration.js')('gtfsHealthCheck', 'MTM Rail: Load Failed!')"
+fi
