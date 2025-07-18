d3dbca42b84e1cb71e812c14ef0500b25d8cea4d
Eyeball Edward <edwardyeung39@gmail.com>
2025-07-18 12:00:33 +1000

    add in stop data

--- modules/op-timetable/test/vline-op-tt.test.mjs
+++ modules/op-timetable/test/vline-op-tt.test.mjs
@@ -7,6 +7,7 @@ import { GetPlatformServicesAPI, VLinePlatformService, VLinePlatformServices } f
 import { matchTrip } from '../load-vline-op-tt.mjs'
 import { LokiDatabaseConnection } from '@transportme/database'
 import td8741GTFS from './sample-data/td8741-gtfs.json' with { type: 'json' }
+import allStops from './sample-data/stops.json' with { type: 'json' }
 
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -19,7 +20,10 @@ describe('The matchTrip function', () => {
   it.only('Matches a V/Line API trip to a GTFS trip', async () => {
     let database = new LokiDatabaseConnection()
     let gtfsTimetables = database.getCollection('gtfs timetables')
+    let stops = database.getCollection('stops')
+
     await gtfsTimetables.createDocument(td8741GTFS)
+    await stops.createDocument(allStops)
 
     let stubAPI = new StubVLineAPI()
       stubAPI.setResponses([ vlineTrips ])
@@ -35,7 +39,7 @@ describe('The matchTrip function', () => {
       expect(departures[0].departureTime.toUTC().toISO()).to.equal('2025-07-18T01:30:00.000Z')
       expect(departures[0].arrivalTime.toUTC().toISO()).to.equal('2025-07-18T02:48:00.000Z')
 
-      let matchingTrip = matchTrip(departures[0], gtfsTimetables)
+      let matchingTrip = matchTrip('20250718', departures[0], database)
       expect(matchingTrip).to.exist
       expect(matchingTrip.tripID).to.equal('48.T0.1-GEL-mjp-8.11.H')
   })
