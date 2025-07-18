eb8bf77ef2fe824359306d7636e87f6a32db6a0c
Eyeball Edward <edwardyeung39@gmail.com>
2025-07-18 13:09:02 +1000

    add early trip pattern

--- modules/op-timetable/test/vline-op-tt.test.mjs
+++ modules/op-timetable/test/vline-op-tt.test.mjs
@@ -14,8 +14,11 @@ const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const vlineTrips = (await fs.readFile(path.join(__dirname, 'sample-data', 'vline-trips.xml'))).toString()
+const td8007Early = (await fs.readFile(path.join(__dirname, 'sample-data', 'td8007-early-pattern.xml'))).toString()
 const td8007Late = (await fs.readFile(path.join(__dirname, 'sample-data', 'td8007-late-pattern.xml'))).toString()
 const td8741 = (await fs.readFile(path.join(__dirname, 'sample-data', 'td8741-pattern.xml'))).toString()
+
+const clone = o => JSON.parse(JSON.stringify(o))
 
 describe('The matchTrip function', () => {
   it('Matches a V/Line API trip to a GTFS trip', async () => {
@@ -23,8 +26,8 @@ describe('The matchTrip function', () => {
     let gtfsTimetables = database.getCollection('gtfs timetables')
     let stops = database.getCollection('stops')
 
-    await gtfsTimetables.createDocument(td8741GTFS)
-    await stops.createDocument(allStops)
+    await gtfsTimetables.createDocument(clone(td8741GTFS))
+    await stops.createDocument(clone(allStops))
 
     let stubAPI = new StubVLineAPI()
       stubAPI.setResponses([ vlineTrips ])
@@ -50,8 +53,8 @@ describe('The matchTrip function', () => {
     let stops = database.getCollection('stops')
     let routes = database.getCollection('routes')
 
-    await stops.createDocuments(allStops)
-    await routes.createDocuments(allRoutes)
+    await stops.createDocuments(clone(allStops))
+    await routes.createDocuments(clone(allRoutes))
 
     let stubAPI = new StubVLineAPI()
       stubAPI.setResponses([ vlineTrips, td8741 ])
@@ -78,13 +81,13 @@ describe('The matchTrip function', () => {
       expect(trip.stopTimings[1].departureTime).to.equal('11:38')
   })
 
-  it.only('Cross references the NSP to handle trips with duplicated stops (late trip)', async () => {
+  it('Cross references the NSP to handle trips with duplicated stops (late trip)', async () => {
     let database = new LokiDatabaseConnection()
     let stops = database.getCollection('stops')
     let routes = database.getCollection('routes')
 
-    await stops.createDocuments(allStops)
-    await routes.createDocuments(allRoutes)
+    await stops.createDocuments(clone(allStops))
+    await routes.createDocuments(clone(allRoutes))
 
     let stubAPI = new StubVLineAPI()
       stubAPI.setResponses([ vlineTrips, td8007Late ])
@@ -96,12 +99,12 @@ describe('The matchTrip function', () => {
       expect(departures[1]).to.be.instanceOf(VLinePlatformService)
       expect(departures[1].tdn).to.equal('8007')
 
-      let matchingTrip = await matchTrip('20250726', departures[0], database)
+      let matchingTrip = await matchTrip('20250726', departures[1], database)
       expect(matchingTrip).to.not.exist
 
-      let pattern = await downloadTripPattern('20250726', departures[0], database)
+      let pattern = await downloadTripPattern('20250726', departures[1], database)
       let trip = pattern.toDatabase()
-      expect(trip.runID).to.equal('8741')
+      expect(trip.runID).to.equal('8007')
       expect(trip.direction).to.equal('Down')
 
       expect(trip.stopTimings[0].stopName).to.equal('Gisborne Railway Station')
@@ -110,5 +113,39 @@ describe('The matchTrip function', () => {
       expect(trip.stopTimings[1].stopName).to.equal('Macedon Railway Station')
       expect(trip.stopTimings[1].arrivalTime).to.equal('08:17')
       expect(trip.stopTimings[1].departureTime).to.equal('08:17')
+  })
+
+  it.only('Cross references the NSP to handle trips with duplicated stops (early trip)', async () => {
+    let database = new LokiDatabaseConnection()
+    let stops = database.getCollection('stops')
+    let routes = database.getCollection('routes')
+
+    await stops.createDocuments(clone(allStops))
+    await routes.createDocuments(clone(allRoutes))
+
+    let stubAPI = new StubVLineAPI()
+      stubAPI.setResponses([ vlineTrips, td8007Early ])
+      let ptvAPI = new PTVAPI(stubAPI)
+      ptvAPI.addVLine(stubAPI)
+
+      let departures = await ptvAPI.vline.getDepartures('', GetPlatformServicesAPI.BOTH, 30)
+
+      expect(departures[2]).to.be.instanceOf(VLinePlatformService)
+      expect(departures[2].tdn).to.equal('8007')
+
+      let matchingTrip = await matchTrip('20250726', departures[2], database)
+      expect(matchingTrip).to.not.exist
+
+      let pattern = await downloadTripPattern('20250726', departures[2], database)
+      let trip = pattern.toDatabase()
+      expect(trip.runID).to.equal('8007')
+      expect(trip.direction).to.equal('Down')
+
+      expect(trip.stopTimings[0].stopName).to.equal('Gisborne Railway Station')
+      expect(trip.stopTimings[0].departureTime).to.equal('07:45')
+
+      expect(trip.stopTimings[1].stopName).to.equal('Macedon Railway Station')
+      expect(trip.stopTimings[1].arrivalTime).to.equal('07:50')
+      expect(trip.stopTimings[1].departureTime).to.equal('07:50')
   })
 })
