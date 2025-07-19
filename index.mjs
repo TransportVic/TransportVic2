4d3ee01043b4646af5c555274b16a0063774ed3f
Eyeball Edward <edwardyeung39@gmail.com>
2025-07-19 20:08:43 +1000

    fix vline mail server

--- index.mjs
+++ index.mjs
@@ -3,8 +3,9 @@ global.startTime = +new Date()
 import _utils from './utils.js'
 import config from './config.json' with { type: 'json' }
 import MainServer from './server/MainServer.mjs'
-import _vlineMail from './modules/vline-mail/index.js'
+import startVLineMailServer from './modules/vline-mail/index.js'
 import _loggers from './init-loggers.mjs'
+import modules from './modules.json' with { type: 'json' }
 
 let mainServer = new MainServer()
 await mainServer.connectToDatabase()
@@ -19,5 +20,7 @@ global.loggers.general.info('Server Started')
 process.on('uncaughtException', err => {
   global.loggers.error.err(err)
 })
+
+if (modules.vlineMail) startVLineMailServer()
 
 console.err = console.error
--- modules/vline-mail.js
+++
@@ -1,5 +0,0 @@
-const modules = require('../modules')
-
-if (modules.vlineMail) {
-  require('./vline-mail/index')()
-}
