9024cc46b94245148f4dd5dc9902c2af6357df48
Eyeball Edward <edwardyeung39@gmail.com>
2025-08-11 07:36:46 +1000

    add overland to op TT

---
+++ modules/op-timetable/load-overland-op-tt.mjs
@@ -0,0 +1,41 @@
+import { MongoDatabaseConnection } from '@transportme/database'
+import config from '../../config.json' with { type: 'json' }
+import utils from '../../utils.js'
+import { convertToLive } from '../departures/sch-to-live.js'
+import { GTFS_CONSTANTS } from '@transportme/transportvic-utils'
+
+const { TRANSIT_MODES } = GTFS_CONSTANTS
+
+let mongoDB = new MongoDatabaseConnection(config.databaseURL, config.databaseName)
+await mongoDB.connect()
+
+let gtfsTimetables = mongoDB.getCollection('gtfs timetables')
+let liveTimetables = mongoDB.getCollection('live timetables')
+
+async function loadOperationalTT(operationDay) {
+  let opDayFormat = utils.getYYYYMMDD(operationDay)
+  let rawActiveTrips = await gtfsTimetables.findDocuments({
+    mode: TRANSIT_MODES.regionalTrain,
+    routeGTFSID: /^10-/,
+    operationDays: opDayFormat
+  }).toArray()
+
+  console.log('Fetched', rawActiveTrips.length, 'trips to process')
+
+  let bulkUpdate = rawActiveTrips.map(trip => convertToLive(trip, operationDay)).map(trip => ({
+    replaceOne: {
+      filter: { mode: trip.mode, operationDays: trip.operationDays, runID: trip.runID },
+      replacement: trip,
+      upsert: true
+    }
+  }))
+
+  if (bulkUpdate.length) {
+    await liveTimetables.bulkWrite(bulkUpdate)
+  }
+}
+
+await loadOperationalTT(utils.now())
+await loadOperationalTT(utils.now().add(1, 'day'))
+
+await mongoDB.close()
