60bf34623da835ec9e9f1a973b4d9bb86362c30f
Eyeball Edward <edwardyeung39@gmail.com>
2025-08-10 22:20:38 +1000

    set RRB run ids when loading coach op TT

--- modules/op-timetable/load-coach-op-tt.mjs
+++ modules/op-timetable/load-coach-op-tt.mjs
@@ -12,24 +12,42 @@ await mongoDB.connect()
 let gtfsTimetables = mongoDB.getCollection('gtfs timetables')
 let liveTimetables = mongoDB.getCollection('live timetables')
 
+let A_CHAR = 'A'.charCodeAt(0)
+
 async function loadOperationalTT(operationDay) {
   let opDayFormat = utils.getYYYYMMDD(operationDay)
   let rawActiveTrips = await gtfsTimetables.findDocuments({
     mode: TRANSIT_MODES.regionalCoach,
     operationDays: opDayFormat
-  }).toArray()
+  }).sort({ departureTime: 1, destinationArrivalTime: 1 }).toArray()
 
   console.log('Fetched', rawActiveTrips.length, 'trips to process')
 
-  // TODO: Implement block data with refactored method
-  let activeTrips = rawActiveTrips.map(trip => convertToLive(trip, operationDay)).map(trip => {
-    trip.operationDays = opDayFormat
-    trip.runID = trip.runID ? `V${trip.runID}` : trip.tripID
+  let rrbTrips = rawActiveTrips.filter(trip => trip.isRailReplacementBus)
+  let schTrips = rawActiveTrips.filter(trip => !trip.isRailReplacementBus)
 
-    return trip
-  })
+  let rrbGroupings = rrbTrips.reduce((acc, trip) => {
+    if (!acc[trip.railRunID]) acc[trip.railRunID] = []
+    acc[trip.railRunID].push(trip)
+    return acc
+  }, {})
 
-  let bulkUpdate = activeTrips.map(trip => ({
+  for (let railRunID of Object.keys(rrbGroupings)) {
+    if (rrbGroupings[railRunID].length === 1) {
+      rrbGroupings[railRunID][0].runID = `${railRunID}C`
+    } else {
+      for (let i = 0; i < rrbGroupings[railRunID].length; i++) {
+        rrbGroupings[railRunID][i].runID = `${railRunID}${String.fromCharCode(A_CHAR + i)}`
+      }
+    }
+  }
+
+  schTrips.forEach(trip => trip.runID = trip.runID ? `V${trip.runID}` : trip.tripID)
+
+  let bulkUpdate = Object.values(rawActiveTrips.reduce((acc, trip) => {
+    acc[trip.runID] = trip
+    return acc
+  }, {})).map(trip => convertToLive(trip, operationDay)).map(trip => ({
     replaceOne: {
       filter: { mode: trip.mode, operationDays: trip.operationDays, runID: trip.runID },
       replacement: trip,
