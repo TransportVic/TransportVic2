69bd07e68f02c715165969b5d09cc79a41d0c408
Eyeball Edward <edwardyeung39@gmail.com>
2025-08-11 12:20:48 +1000

    implement platform usage based on arrivals

--- modules/metro-trains/platform-usage.mjs
+++ modules/metro-trains/platform-usage.mjs
@@ -1,5 +1,20 @@
+import utils from '../../utils.js'
 import getMetroDepartures from './get-departures.js'
 
 export async function getPlatformUsage(db, station, time) {
-  let departures = await getMetroDepartures(station, db, null, null, time, { returnArrivals: true, timeframe: 30 })
+  let startTime = utils.parseTime(time).add(-30, 'minutes')
+  let trips = await getMetroDepartures(station, db, null, null, startTime, { returnArrivals: true, timeframe: 60 })
+  let allTrips = trips.reduce((acc, e) => {
+    acc[e.runID] = e
+    return acc
+  }, {})
+
+  let arrivals = trips.filter(trip => trip.isArrival)
+  let arrivalPlatformUsage = arrivals.map(trip => ({
+    runID: trip.runID,
+    start: trip.scheduledDepartureTime,
+    end: trip.formingTrip && allTrips[trip.formingRunID] ? allTrips[trip.formingRunID].scheduledDepartureTime : trip.scheduledDepartureTime.clone().add(2, 'minutes')
+  }))
+
+  return arrivalPlatformUsage
 }
--- test/metro/utils/platform-usage.test.mjs
+++ test/metro/utils/platform-usage.test.mjs
@@ -1,3 +1,4 @@
+import { expect } from 'chai'
 import { LokiDatabaseConnection } from '@transportme/database'
 import { getPlatformUsage } from '../../../modules/metro-trains/platform-usage.mjs'
 import pkmStopsDB from '../tracker/sample-data/pkm-stops-db.json' with { type: 'json' }
@@ -6,7 +7,7 @@ import ephTrips from './sample-data/eph-trips.mjs'
 const clone = o => JSON.parse(JSON.stringify(o))
 
 describe('The platform usage function', () => {
-  it('Returns a list of times that trains sit on the platforms for', async () => {
+  it.only('Returns a list of times that trains sit on the platforms for', async () => {
     let database = new LokiDatabaseConnection()
     let stops = database.getCollection('stops')
     let timetables = database.getCollection('live timetables')
@@ -17,8 +18,8 @@ describe('The platform usage function', () => {
 
     let platformUsage = await getPlatformUsage(database, eph, new Date('2025-08-11T01:48:00.000Z'))
 
-    let dwell_4043_4070 = platformUsage.find(dwell => dwell.runID === '4070')
-    let dwell_4045_4072 = platformUsage.find(dwell => dwell.runID === '4072')
+    let dwell_4043_4070 = platformUsage.find(dwell => dwell.runID === '4043')
+    let dwell_4045_4072 = platformUsage.find(dwell => dwell.runID === '4045')
 
     expect(dwell_4043_4070.start.toISOString()).to.equal('2025-08-11T01:27:00.000Z') // 11:27
     expect(dwell_4043_4070.end.toISOString()).to.equal('2025-08-11T01:43:00.000Z') // 11:43
