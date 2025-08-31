bf7df7404264e2f8f7bed26dfd3f9b2bbc918698
Eyeball Edward <edwardyeung39@gmail.com>
2025-08-31 18:51:21 +1000

    group RRB by route to avoid wiping data on lines that werent programmed in

--- load-gtfs/metro-rail-bus/load-op-timetable.mjs
+++ load-gtfs/metro-rail-bus/load-op-timetable.mjs
@@ -2,6 +2,12 @@ import { MongoDatabaseConnection } from '@transportme/database'
 import config from '../../config.json' with { type: 'json' }
 import utils from '../../utils.js'
 import { convertToLive } from '../../modules/departures/sch-to-live.js'
+import routeGroups from './route-groups.json' with { type: 'json' }
+
+const routeGroupings = Object.keys(routeGroups).reduce((acc, e) => ({
+  ...acc,
+  [routeGroups[e]]: acc[routeGroups[e]] ? acc[routeGroups[e]].concat(e) : [e]
+}), {})
 
 let mongoDB = new MongoDatabaseConnection(config.databaseURL, config.databaseName)
 await mongoDB.connect()
@@ -26,7 +32,20 @@ async function loadOperationalTT(operationDay) {
 
   // Delete all RRB data and use this only if there is actually data
   // Also means that if MTM data drops out but PTV data is still there we don't accidentally delete it
-  if (activeTrips.length) {
+  // Note: Group into line groups as we sometimes have data programmed in on some lines but not others
+  let tripGroups = Object.values(routeGroups).reduce((acc, grp) => ({
+    ...acc,
+    [grp]: []
+  }), {})
+
+  for (let trip of activeTrips) {
+    tripGroups[routeGroups[trip.routeName]].push(trip)
+  }
+
+  for (let group of Object.keys(tripGroups)) {
+    let trips = tripGroups[group]
+    if (!trips.length) continue
+
     let hasRealtimeData = await liveTimetables.distinct('tripID', {
       mode: 'metro train',
       operationDays: opDayFormat,
@@ -40,7 +59,7 @@ async function loadOperationalTT(operationDay) {
       }
     })
 
-    let replacementTrips = activeTrips.filter(trip => !hasRealtimeData.includes(trip.tripID))
+    let replacementTrips = trips.filter(trip => !hasRealtimeData.includes(trip.tripID))
     let bulkWrite = replacementTrips.map(trip => ({
       replaceOne: {
         filter: {
@@ -59,6 +78,9 @@ async function loadOperationalTT(operationDay) {
       isRailReplacementBus: true,
       routeGTFSID: {
         $ne: '2-RRB'
+      },
+      routeName: {
+        $in: routeGroupings[group]
       }
     })
     await liveTimetables.bulkWrite(bulkWrite)
