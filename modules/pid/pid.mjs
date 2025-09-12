cbde5321864172440db82662f9ae13e4b43ac0a4
Eyeball Edward <edwardyeung39@gmail.com>
2025-09-12 13:43:52 +1000

    return very basic list of departures

--- modules/pid/pid.mjs
+++ modules/pid/pid.mjs
@@ -1,3 +1,9 @@
-export async function getPIDDepartures(stationName, db, { departureTime = new Date() } = {}) {
+import getMetroDepartures from '../metro-trains/get-departures.js'
 
+export async function getPIDDepartures(stationName, db, { departureTime = new Date() } = {}) {
+  const stops = await db.getCollection('stops')
+  const stationData = await stops.findDocument({ stopName: `${stationName} Railway Station` })
+  const metroDepartures = await getMetroDepartures(stationData, db, false, false, departureTime)
+
+  return metroDepartures
 }
