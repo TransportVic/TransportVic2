76b61769050bb92fa8a6e4bf611bb7abadac0bc1
Eyeball Edward <edwardyeung39@gmail.com>
2025-10-25 15:23:04 +1100

    update highlights

--- additional-data/bus-tracker/highlights.js
+++ additional-data/bus-tracker/highlights.js
@@ -1,16 +1,16 @@
 module.exports = [{
   name: 'Ventura Minibuses',
-  track: ['V376', 'V377', 'V715', 'V716', 'V717', 'V718', 'V719', 'V720', 'V721', 'V722', 'V724', 'V736', 'V740', 'V741', 'V744', 'V745', 'V746', 'V826', 'V1074', 'V1075', 'V1076', 'V1077', 'V1079', 'V1080', 'V1081', 'V1082', 'V1083', 'V1084', 'V1085', 'V1086', 'V1087', 'V1268', 'V1269', 'V1270', 'V1310', 'V1311', 'V1377', 'V1408'],
+  track: ['V376', 'V377', 'V1268', 'V1269', 'V1270', 'V1310', 'V1311', 'V1377', 'V1408'],
   routes: ['526', '527', '548', '549', '550', '551', '671', '672', '673', '675', '676', '677', '680', '786', '787', '886', '887', 'TB1', 'TB2', 'TB3', 'TB4', 'TB7', 'TB8', 'TB9'],
   type: 'exclude'
 }, {
   name: 'Ventura Artics',
   track: ['V100', 'V102', 'V136', 'V143', 'V190', 'V292', 'V293', 'V294', 'V730', 'V732', 'V733', 'V734', 'V735', 'V842', 'V1271', 'V1374', 'V1378', 'V1379', 'V1380', 'V1422', 'V1423', 'V1424', 'V1426', 'V1427', 'V1463', 'V1464', 'V1466', 'V1470', 'V1478', 'V1479'],
-  routes: ['811', '812', '813', '664', '670', '679', '788'],
+  routes: ['811', '812', '813', '664', '670', '679', '788', '887'],
   type: 'exclude'
 }, {
   name: 'Ventura Specials',
-  track: ['V21', 'V136', 'V143', 'V200', 'V287', 'V322', 'V343', 'V730', 'V734', 'V759', 'V760', 'V784', 'V785', 'V786', 'V787', 'V789', 'V792', 'V795', 'V796', 'V1081', 'V1097', 'V1098', 'V1175', 'V1176', 'V1182', 'V1184', 'V1194', 'V1195', 'V1380', 'V1445'],
+  track: ['V143', 'V292', 'V294', 'V343', 'V895', 'V1380', 'V1445', 'V8239'],
   routes: [],
   type: 'exclude'
 }, {
@@ -19,76 +19,25 @@ module.exports = [{
   routes: ['735', '736', '765'],
   type: 'include'
 }, {
-  name: 'Ventura B10BLE',
-  track: ['V10', 'V30', 'V31', 'V34', 'V35', 'V43', 'V44', 'V78', 'V114', 'V116', 'V137', 'V138', 'V139', 'V304'],
-  routes: [],
-  type: 'exclude'
-}, {
-  name: 'Ventura O405NH',
-  track: ['V187', 'V197', 'V198', 'V529', 'V548', 'V550', 'V569', 'V570'],
-  routes: [],
-  type: 'exclude'
-}, {
   name: 'Kinetic Minibuses',
-  track: ['T119', 'T120', 'T129', 'T130', 'T131', 'T132', 'T133', 'T183'],
-  routes: ['280', '282', '370'],
+  track: ['K119', 'K120', 'K129', 'K130', 'K131', 'K132', 'K133', 'K183'],
+  routes: ['280', '282', '370', '509'],
   type: 'exclude'
 }, {
-  name: 'Kinetic Specials',
-  track: ['T703', 'T705', 'T708', 'T710', 'T712', 'T736', 'T737', 'T738', 'T739'],
-  routes: [],
-  type: 'exclude'
-}, {
-  name: 'Cranbourne O405NH',
-  track: ['CR32', 'CR33', 'CR34', 'CR35'],
-  routes: ['795', '891', '894', '895'],
-  type: 'include'
-}, {
-  name: 'Cranbourne Specials',
-  track: ['CR3', 'CR9', 'CR11', 'CR12', 'CR13', 'CR37'],
-  routes: [],
-  type: 'exclude'
-}, {
-  name: 'Route Livery Buses',
-  track: ['TS16' ,'TS19', 'TS27', 'TS40', 'TS41', 'TS43', 'TS54', 'TS71', 'CO107', 'CO108', 'CO127', 'CO128', 'CO129', 'CO130', 'CO154', 'CO171', 'CO175', 'CO176', 'D897', 'D898', 'D899', 'D900', 'V1255', 'V1256'],
+  name: 'Uni Shuttle Buses',
+  track: ['TS16' ,'TS19', 'TS27', 'TS40', 'TS41', 'TS43', 'TS54', 'TS71', 'CO13', 'CO15', 'CO23', 'CO24', 'CO127', 'CO128', 'CO129', 'CO130', 'CO154', 'CO171', 'CO175', 'D897', 'D898', 'D899', 'D900', 'V1255', 'V1256'],
   routes: ['201', '301', '401', '601'],
   type: 'exclude'
 }, {
-  name: 'Non-Perm Route Livery Buses',
-  track: ['TS16' ,'TS19', 'TS27', 'TS40', 'TS41', 'TS43', 'TS54', 'TS71', 'CO107', 'CO108', 'CO127', 'CO128', 'CO129', 'CO130', 'CO154', 'CO171', 'CO175', 'CO176', 'D897', 'D898', 'D899', 'D900', 'V1255', 'V1256'],
+  name: 'Non-Perm Uni Shuttle Buses',
+  track: ['TS16' ,'TS19', 'TS27', 'TS40', 'TS41', 'TS43', 'TS54', 'TS71', 'CO13', 'CO15', 'CO23', 'CO24', 'CO127', 'CO128', 'CO129', 'CO130', 'CO154', 'CO171', 'CO175', 'D897', 'D898', 'D899', 'D900', 'V1255', 'V1256'],
   routes: ['201', '301', '401', '601'],
   type: 'include',
   buses: 'exclude'
-}, {
-  name: 'Old Sita',
-  track: ['TS33', 'TS64', 'TS80', 'TS81', 'TS86', 'TS91', 'TS92', 'TS93', 'TS94', 'TS95', 'TS97', 'TS99'],
-  routes: [],
-  type: 'exclude'
-}, {
-  name: '900 Perms off 900',
-  track: ['CO116', 'CO117', 'CO118', 'CO119', 'CO120', 'CO121', 'CO146', 'CO147', 'V1309', 'V1323', 'V8227', 'V8228', 'V8229', 'V8230', 'V8231', 'V8235', 'V8239', 'V8909'],
-  routes: ['900'],
-  type: 'exclude'
-}, {
-  name: 'Non-900 Perms on 900',
-  track: ['CO116', 'CO117', 'CO118', 'CO119', 'CO120', 'CO121', 'CO146', 'CO147', 'V1309', 'V1323', 'V8227', 'V8228', 'V8229', 'V8230', 'V8231', 'V8235', 'V8239', 'V8909'],
-  routes: ['900'],
-  type: 'include',
-  buses: 'exclude'
-}, {
-  name: '703 Perms off 703',
-  track: ['V868', 'V869', 'V877', 'V878', 'V879', 'V880', 'V881', 'V882', 'V883', 'V884', 'V1237', 'V1238', 'V1239', 'V1240', 'V1241', 'V1242'],
-  routes: ['703'],
-  type: 'exclude'
 }, {
   name: 'CDC Oakleigh Specials',
   track: ['CO7', 'CO8', 'CO11', 'CO12', 'CO13', 'CO34', 'CO100', 'CO103', 'CO104', 'CO105', 'CO109'],
   routes: [],
-  type: 'exclude'
-}, {
-  name: 'CDC Oakleigh Hybrids',
-  track: ['CO152', 'CO157', 'CO158', 'CO159', 'CO160', 'CO161', 'CO162', 'CO163'],
-  routes: ['601', '605', '630'],
   type: 'exclude'
 }, {
   name: 'CDC Oakleigh Ex-Drivers',
@@ -96,18 +45,13 @@ module.exports = [{
   routes: ['605'],
   type: 'include'
 }, {
-  name: 'CDC Tullamarine Specials',
-  track: ['CT3', 'CT4', 'CT7', 'CT8', 'CT12', 'CT13', 'CT17', 'CT18', 'CT20'],
+  name: 'CDC Tulla ine Specials',
+  track: ['CT53'],
   routes: [],
   type: 'exclude'
 }, {
   name: 'CDC Wyndham Specials',
-  track: ['CW20', 'CW21', 'CW22', 'CW23', 'CW76', 'CW79', 'CW80', 'CW126', 'CW127', 'CW131', 'CW189', 'CW190', 'CW195', 'CW196'],
-  routes: [],
-  type: 'exclude'
-}, {
-  name: 'CDC Sunshine Specials',
-  track: ['CS53', 'CS54', 'CS57', 'CS58', 'CS62'],
+  track: ['CW10', 'CW338', 'CW447'],
   routes: [],
   type: 'exclude'
 }, {
@@ -118,11 +62,6 @@ module.exports = [{
 }, {
   name: 'CDC Geelong Specials',
   track: ['CG92', 'CG93', 'CG94', 'CG95', 'CG97', 'CG98', 'CG99', 'CG100', 'CG101', 'CG102'],
-  routes: [],
-  type: 'exclude'
-}, {
-  name: 'Kinetic Specials',
-  track: ['T574', 'T585'],
   routes: [],
   type: 'exclude'
 }, {
@@ -142,33 +81,18 @@ module.exports = [{
   type: 'exclude'
 }, {
   name: '509 Perms off 509',
-  track: ['D895', 'D896'],
+  track: ['K221', 'K222'],
   routes: ['505', '509'],
   type: 'exclude'
 }, {
   name: 'Non-509 Perms on 509',
-  track: ['D895', 'D896'],
+  track: ['K221', 'K222'],
   routes: ['509'],
   type: 'include',
   buses: 'exclude'
 }, {
   name: 'McHarry Specials',
   track: ['MH32', 'MH43', 'MH51', 'MH55', 'MH56', 'MH65', 'MH89', 'MH127', 'MH140', 'MH144', 'MH145', 'MH147', 'MH150'],
-  routes: [],
-  type: 'exclude'
-}, {
-  name: 'Moonee Valley Specials',
-  track: ['MV71', 'MV72', 'MV77'],
-  routes: [],
-  type: 'exclude'
-}, {
-  name: 'Panorama Specials',
-  track: ['P3', 'P5', 'P7', 'P19', 'P47'],
-  routes: [],
-  type: 'exclude'
-}, {
-  name: 'Ryan\'s Specials',
-  track: ['R40', 'R41', 'R42'],
   routes: [],
   type: 'exclude'
 }, {
@@ -182,8 +106,13 @@ module.exports = [{
   routes: [],
   type: 'exclude'
 }, {
-  name: 'Whitemore Specials',
-  track: ['WM6695'],
+  name: 'Seymour Specials',
+  track: ['SY68'],
+  routes: [],
+  type: 'exclude'
+}, {
+  name: 'Martyr\'s Specials',
+  track: ['MT24', 'MT25'],
   routes: [],
   type: 'exclude'
 }]
