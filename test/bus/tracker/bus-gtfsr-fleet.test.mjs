d17f0e4c0f3bd66299cc84a12b414b4da8cbc4f2
Eyeball Edward <edwardyeung39@gmail.com>
2025-10-25 14:22:43 +1100

    add tracked rego test

--- test/bus/tracker/bus-gtfsr-fleet.test.mjs
+++ test/bus/tracker/bus-gtfsr-fleet.test.mjs
@@ -14,4 +14,21 @@ describe('The bus fleet tracker', () => {
     expect(trips[0].routeGTFSID).to.equal('4-733')
     expect(trips[0].consist[0]).to.equal('BS12YD')
   })
+
+  it('Handles vehicle rego overrides', async () => {
+    let database = new LokiDatabaseConnection()
+    const busRegos = database.getCollection('bus regos')
+    busRegos.createDocument({
+      fleetNumber: "V1678",
+      operator: "V",
+      rego: "TEST",
+      trackedRego: "BS12YD"
+    })
+
+    const trips = await getFleetData(database, () => gtfsr733Positions)
+    expect(trips[0].operationDays).to.equal('20251025')
+    expect(trips[0].runID).to.equal('20-733--1-Sat2-31')
+    expect(trips[0].routeGTFSID).to.equal('4-733')
+    expect(trips[0].consist[0]).to.equal('TEST')
+  })
 })
