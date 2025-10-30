1e425c9d38fa97b3d2f27e3a737bbcfe595e8f94
Eyeball Edward <edwardyeung39@gmail.com>
2025-10-30 17:40:03 +1100

    update log rotation utility

---
+++ scripts/rotate-logs.mjs
@@ -0,0 +1,50 @@
+import config from '../config.json' with { type: 'json' }
+import { MongoDatabaseConnection } from '@transportme/database'
+import fs from 'fs/promises'
+import path from 'path'
+import url from 'url'
+import os from 'os'
+import utils from '../utils.js'
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const tripDatabaseURL = new URL(config.tripDatabaseURL)
+tripDatabaseURL.host = os.hostname()
+tripDatabaseURL.searchParams.set('directConnection', 'true')
+
+const database = new MongoDatabaseConnection(config.databaseURL, 'admin')
+await database.connect({})
+await database.adminCommand({ logRotate: 'server' })
+
+const tripDatabase = new MongoDatabaseConnection(tripDatabaseURL.toString(), 'admin')
+await tripDatabase.connect({})
+await tripDatabase.adminCommand({ logRotate: 'server' })
+
+const now = utils.now()
+
+for (const folder of config.databaseLog) {
+  const files = await fs.readdir(folder)
+  for (const file of files) {
+    if (file.endsWith('.log')) continue
+    const timeData = file.slice(-19)
+    const date = `${timeData.slice(0, 10)}T${timeData.slice(11).replace(/-/g, ':')}Z`
+    const time = utils.parseTime(date)
+
+    if (!time || now.diff(time, 'days') > 14) {
+      await fs.unlink(path.join(folder, file))
+    }
+  }
+}
+
+const logDir = path.join(__dirname, '..', '..', 'logs')
+const logFiles = await fs.readdir(logDir)
+
+for (const log of logFiles) {
+  const date = utils.parseDate(log)
+  if (!date || now.diff(date, 'days') > 14) {
+    await fs.rm(path.join(logDir, log), { recursive: true })
+  }
+}
+
+process.exit(0)
--- scripts/rotate-logs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DIRNAME=$(dirname "$0")
-
-cd $DIRNAME/..
-. ./.env
-
-sudo chmod a+rw -R /var/log/mongodb/
-node $DIRNAME/js/rotate-logs.mjs
