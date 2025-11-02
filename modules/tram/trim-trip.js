382fa24f598d040f11b174a0fc9cefffc2ec09e3
Eyeball Edward <edwardyeung39@gmail.com>
2025-11-02 16:02:11 +1100

    add 48 north balwyn override

--- modules/tram/trim-trip.js
+++ modules/tram/trim-trip.js
@@ -55,6 +55,7 @@ module.exports.trimFromDestination = async function(db, destination, coreRoute, 
   let destinationWithoutRoad = searchDestination.replace(/ *&.*/, ' &')
 
   if (coreRoute === '70' && searchDestination === 'Wattle Park') return trip
+  if (coreRoute === '48' && searchDestination === 'North Balwyn') return trip
 
   await async.forEachOf(trip.stopTimings, async (stop, i) => {
     if (!cutoffStop) {
