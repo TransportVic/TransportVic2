7a7366bd994fdbe5d41cd7284ea209139d6f15df
Eyeball Edward <edwardyeung39@gmail.com>
2025-11-28 16:07:26 +0800

    add bus tracker tests

---
+++ test/tracker/bus-tracker.test.mjs
@@ -0,0 +1,67 @@
+import { expect, use } from 'chai'
+import chaiExclude from 'chai-exclude'
+import utils from '../../utils.js'
+import { LokiDatabaseConnection } from '@transportme/database'
+import busTrips from './sample-data/bus-trips.mjs'
+import BusTracker from '../../application/model/tracker/BusTracker.mjs'
+import busRegos from './sample-data/bus-regos.mjs'
+use(chaiExclude)
+
+const clone = o => JSON.parse(JSON.stringify(o))
+
+describe('The BusTracker class', () => {
+  it('Searches by fleet number', async () => {
+    const db = new LokiDatabaseConnection()
+    const tripsColl = db.getCollection('bus trips')
+    const regosColl = db.getCollection('bus regos')
+    await tripsColl.createDocuments(clone(busTrips))
+    await regosColl.createDocuments(clone(busRegos))
+
+    const tracker = new BusTracker(db)
+    const trips = await tracker.getTripsByFleet('CO171', { date: '20251128' })
+    expect(trips[0].runID).to.equal('49-601--MF-1591010')
+    expect(trips[1].runID).to.equal('49-601--MF-1591110')
+  })
+
+  it('Searches by rego', async () => {
+    const db = new LokiDatabaseConnection()
+    const tripsColl = db.getCollection('bus trips')
+    const regosColl = db.getCollection('bus regos')
+    await tripsColl.createDocuments(clone(busTrips))
+    await regosColl.createDocuments(clone(busRegos))
+
+    const tracker = new BusTracker(db)
+    const trips = await tracker.getTripsByFleet('1013AO', { date: '20251128' })
+    expect(trips[0].runID).to.equal('49-601--MF-1602110')
+    expect(trips[1].runID).to.equal('49-601--MF-1602210')
+  })
+
+  it('Adds fleet data when searching by route number', async () => {
+    const db = new LokiDatabaseConnection()
+    const tripsColl = db.getCollection('bus trips')
+    const regosColl = db.getCollection('bus regos')
+    await tripsColl.createDocuments(clone(busTrips))
+    await regosColl.createDocuments(clone(busRegos))
+
+    const tracker = new BusTracker(db)
+    const trips = await tracker.getTripsByRoute('601', { date: '20251128' })
+    const trip1 = trips.find(t => t.runID === '49-601--MF-1602110')
+    expect(trip1.displayConsist).to.equal('#CO13')
+
+    const trip2 = trips.find(t => t.runID === '49-601--MF-1591010')
+    expect(trip2.displayConsist).to.equal('#CO171')
+  })
+
+  it('Retains the rego when the fleet number cannot be determined', async () => {
+    const db = new LokiDatabaseConnection()
+    const tripsColl = db.getCollection('bus trips')
+    const regosColl = db.getCollection('bus regos')
+    await tripsColl.createDocuments(clone(busTrips))
+    await regosColl.createDocuments(clone(busRegos))
+
+    const tracker = new BusTracker(db)
+    const trips = await tracker.getTripsByRoute('601', { date: '20251127' })
+    const trip1 = trips.find(t => t.runID === '49-601--MF-1602110')
+    expect(trip1.displayConsist).to.equal('@7509AO')
+  })
+})
