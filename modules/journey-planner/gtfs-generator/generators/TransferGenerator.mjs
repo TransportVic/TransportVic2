0ea1728abf680684e33af96be3e95af613c24a56
Eyeball Edward <edwardyeung39@gmail.com>
2025-12-18 22:58:16 +0800

    shorten route IDs

--- modules/journey-planner/gtfs-generator/generators/TransferGenerator.mjs
+++ modules/journey-planner/gtfs-generator/generators/TransferGenerator.mjs
@@ -5,4 +5,13 @@ export default class TransferGenerator extends CopyGenerator {
   getHeader() { return 'from_stop_id,to_stop_id,from_route_id,to_route_id,from_trip_id,to_trip_id,transfer_type,min_transfer_time' }
   getFileName() { return 'transfers.txt' }
 
+  processColumn(column, folder, line) {
+    const data = line[column]
+    if (column === 'from_route_id' || column === 'to_route_id') {
+      return data.replace(/aus:vic:vic-0?/, '').replace(/-mjp-\d+/, '')
+    }
+
+    return data
+  }
+
 }
