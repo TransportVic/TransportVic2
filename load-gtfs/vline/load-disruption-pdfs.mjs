bb16f1c97d384d050bbeba368c56a2779cbada89
Eyeball Edward <edwardyeung39@gmail.com>
2026-01-11 17:52:05 +1100

    actually add loader file

---
+++ load-gtfs/vline/load-disruption-pdfs.mjs
@@ -0,0 +1,23 @@
+import path from 'path'
+import scripts from '../../transportvic-data/rail-pdfs/scripts.mjs'
+import { spawn } from 'child_process'
+
+const file = path.join(import.meta.dirname, '../..', 'scripts', 'load-vline-tt.mjs')
+
+for (const command of scripts.commands) {
+  await new Promise(resolve => {
+    console.log('node', [ file, ...command ].join(' '))
+
+    const process = spawn('node', [ file, ...command ], {
+      cwd: scripts.cwd
+    })
+
+    process.stdout.pipe(process.stdout)
+    process.stderr.pipe(process.stderr)
+
+    process.on('close', code => {
+      console.log('Finished with code', code)
+      resolve()
+    })
+  })
+}
