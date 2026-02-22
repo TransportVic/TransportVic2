eac6de4b18690c2b2f2dafd268957699a7cd8815
Eyeball Edward <edwardyeung39@gmail.com>
2026-02-22 11:28:05 +1100

    continue to patch monbulk trips

--- load-gtfs/bus/patch-eastern-trips.mjs
+++ load-gtfs/bus/patch-eastern-trips.mjs
@@ -6,35 +6,29 @@ await database.connect()
 
 const gtfsTimetables = await database.getCollection('gtfs-gtfs timetables')
 
-const depots = [ 19, 22, 29 ]
+const depots = [ 22 ]
 
 const missingRoutes = [
-  '4-663', '4-664', '4-670', '4-671',
-  '4-672', '4-675', '4-677', '4-679',
-  '4-680', '4-682', '4-688', '4-689',
-  '4-690', '4-691', '4-693', '4-694',
-  '4-695', '4-696', '4-697', '4-699',
-  '4-732', '4-735', '4-736', '4-737',
-  '4-738', '4-740', '4-742', '4-753',
-  '4-754', '4-755', '4-765'
+  '4-693', '4-695', '4-696',
+  '4-697', '4-732'
 ]
 
 const missingWeekdayDates = [
-  '20260202', '20260203', '20260204', '20260205', '20260206',
-  '20260209', '20260210', '20260211', '20260212', '20260213'
+  '20260223', '20260224', '20260225', '20260226', '20260227',
+  '20260302', '20260303', '20260304', '20260305', '20260306'
 ]
 
 const missingSaturdayDates = [
-  '20260207', '20260214'
+  '20260228', '20260307'
 ]
 
 const missingSundayDates = [
-  '20260207', '20260214'
+  '20260301', '20260308'
 ]
 
-const refWeekday = '20260302'
-const refSaturday = '20260314'
-const refSunday = '20260315'
+const refWeekday = '20260323'
+const refSaturday = '20260328'
+const refSunday = '20260329'
 
 async function expandDay(refDay, missingDays) {
   const trips = await gtfsTimetables.findDocuments({
